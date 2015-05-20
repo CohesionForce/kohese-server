3dea269b7b6186841ce0b3896166890fa7a329bf
dphillips <dphillips@CohesionForce.com>
2015-05-20 16:06:07 -0500

    Moved detail form to launch on id instead of icon. Added highlighting of search results. Increased default size of description text area.

--- kohese-portal/client/js/app.js
+++ kohese-portal/client/js/app.js
@@ -31,4 +31,12 @@ angular
 
     $urlRouterProvider
       .otherwise('/');
-  }]);
+  }])
+    .filter('highlight', function($sce) {
+    return function(text, phrase) {
+      if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
+        '<span class="highlighted">$1</span>')
+
+      return $sce.trustAsHtml(text)
+    }
+  });
