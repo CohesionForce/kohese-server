86f25a6276ef295120cffc96f8f20903a6a42bd8
dphillips <dphillips@CohesionForce.com>
2015-05-27 13:48:41 -0500

    Initial scaffolding to catch Item changes and emitting via socket.io.

--- kohese-portal/common/models/item.js
+++ kohese-portal/common/models/item.js
@@ -1,3 +1,19 @@
 module.exports = function(Item) {
 
+	//var io = require('socket.io');
+	//var app = require('loopback');
+	
+	Item.observe('after save', function(ctx, next) {
+		  if (ctx.instance) {
+		    console.log('Saved %s #%s#%s#', ctx.Model.modelName, ctx.instance.id, ctx.instance.title);
+		    console.log(JSON.stringify(ctx));
+//		    app.io.emit('chat message', 'hello');
+		  } else {
+		    console.log('Updated %s matching %j',
+		      ctx.Model.pluralModelName,
+		      ctx.where);
+		  }
+		  next();
+		});
+	
 };
