e7c3812e58de5a3b81b9b886b6d15be424d474b2
dphillips <dphillips@CohesionForce.com>
2015-05-28 15:50:28 -0500

    Updated change notification to call out type of change and model instance that is being modified.

--- kohese-portal/common/models/item.js
+++ kohese-portal/common/models/item.js
@@ -7,12 +7,53 @@ module.exports = function(Item) {
 	Item.observe('after save', function(ctx, next) {
 		  if (ctx.instance) {
 		    console.log('Saved %s #%s#%s#', ctx.Model.modelName, ctx.instance.id, ctx.instance.title);
-		    console.log(JSON.stringify(ctx));
-		    app.io.emit('change', JSON.stringify(ctx));
+		    var notification = new Object;
+		    if (ctx.isNewInstance) {
+			    notification.type = 'create';
+
+		    } else {
+			    notification.type = 'update';		    	
+		    }
+		    notification.model = ctx.Model.modelName;
+		    notification.id = ctx.instance.id;
+		    notification.ctx = ctx;
+		    console.log(JSON.stringify(notification));
+		    app.io.emit('change', JSON.stringify(notification));
 		  } else {
 		    console.log('Updated %s matching %j',
 		      ctx.Model.pluralModelName,
 		      ctx.where);
+		    var notification = new Object;
+		    if (ctx.isNewInstance) {
+			    notification.type = 'create';
+
+		    } else {
+			    notification.type = 'update';		    	
+		    }
+		    notification.model = ctx.Model.modelName;
+		    notification.id = ctx.where.id;
+		    notification.ctx = ctx;
+		    console.log(JSON.stringify(notification));
+		    app.io.emit('change', JSON.stringify(notification));
+		  }
+		  next();
+		});
+	
+	Item.observe('after delete', function(ctx, next) {
+		  if (ctx.instance) {
+		    console.log('Deleted %s #%s#', ctx.Model.modelName, ctx);
+			notification.type = 'delete';		    	
+		    notification.model = ctx.Model.modelName;
+		    notification.ctx = ctx;
+		  } else {
+		    console.log('Deleted %s #%s#', ctx.Model.modelName, ctx.where);
+		    var notification = new Object;
+			notification.type = 'delete';		    	
+		    notification.model = ctx.Model.modelName;
+		    notification.id = ctx.where.id;
+		    notification.ctx = ctx;
+		    console.log(JSON.stringify(notification));
+		    app.io.emit('change', JSON.stringify(notification));
 		  }
 		  next();
 		});
