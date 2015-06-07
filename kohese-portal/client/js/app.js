b279b80e1733fa513af1323ab66340d20658b409
dphillips <dphillips@CohesionForce.com>
2015-06-06 21:42:26 -0500

    Added check for null text.

--- kohese-portal/client/js/app.js
+++ kohese-portal/client/js/app.js
@@ -49,9 +49,11 @@ angular
   }])
     .filter('highlight', function($sce) {
     return function(text, phrase) {
-      if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
-        '<span class="highlighted">$1</span>')
+      if(text !== null){
+          if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
+          '<span class="highlighted">$1</span>');    	 
+      }
 
-      return $sce.trustAsHtml(text)
+      return $sce.trustAsHtml(text);
     }
   });
