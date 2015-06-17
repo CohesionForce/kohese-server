2d921c7bdeafc21d57b1a6192ecddc3ac80e2d03
dphillips <dphillips@CohesionForce.com>
2015-06-17 15:33:29 -0500

    Refactored code to remove grid-tree-directive.

--- kohese-portal/client/js/item/controllers.js
+++ kohese-portal/client/js/item/controllers.js
@@ -4,36 +4,12 @@ angular
   .module('app')
   .controller('ItemController', ['$scope', '$state', '$location', 'Item', 'ItemRepository', '$anchorScroll',
     function($scope, $state, $location, Item, ItemRepository, $anchorScroll) {
-    $scope.items = [];
-    $scope.tree_data = [];
-    $scope.tree = [];
-    $scope.tree_control = {};
+
     $scope.filterString = "";
     $scope.hidden = {};
     $scope.collapsed = {};
 
     $scope.newTree = ItemRepository.internalTree;
-
-    $scope.listTitle = "Item List"
-    function getItems() {
-      Item
-        .find()
-        .$promise
-        .then(function(results) {
-          $scope.items = results;
-          $scope.tree = convertListToTree($scope.items, 'id', 'parentId');
-          $scope.tree_data = $scope.tree.data;
-        });
-    }
-    getItems();
-
-    $scope.cancel = function() {
-      if (angular.isDefined($state.params.parentId)){
-        $state.go('editItem', {itemId: $state.params.parentId});
-   	  } else {
-        $state.go('items');
-       }
-     }
 
     $scope.newItem = function() {
         $state.go('newItem');
@@ -41,10 +17,6 @@ angular
 
     $scope.editItem = function(item) {
         $state.go('editItem', {itemId: item.id});
-      };
-
-    $scope.editItemById = function(id) {
-        $state.go('editItem', {itemId: id});
       };
 
     $scope.removeItem = function(item) {
@@ -56,104 +28,45 @@ angular
         });
     };
 
-    $scope.expanding_property = {
-        field: "title",
-        displayName: "Title",
-    	sortable : true,
-    	filterable: true,
-		sortingType : "string"
-    };
+    $scope.$on('currentItemUpdate', function(event, data){
+        console.log(event);
+        console.log(data.id);
+        $location.hash(data.id);
+        $anchorScroll();
+      }
+    );
 
-    $scope.col_defs = [
-        {
-            field: "description",
-            displayName: "Description",
-    		sortable : false,
-    		cellTemplate: "<span>{{ row.branch[col.field] | limitTo:70 }}<span ng-show=\"row.branch[col.field].length>70\">...</span></span>",
-    		filterable: true
-        }
-    ];
-
-    $scope.item_click_handler = function (branch) {
-        $state.go('editItem', {itemId: branch.data.id});
-    }
-
-    function convertListToTree(dataList, primaryIdName, parentIdName) {
-        if (!dataList || dataList.length == 0 || !primaryIdName || !parentIdName)
-            return [];
-
-        var rootIds = [],
-            primaryKey,
-            parentId,
-            parent,
-            len = dataList.length,
-            i = 0;
-
-        var tree = {};
-        tree.data = [];
-        tree.objs = {};
-
-        while (i < len) {
-        	var itemProxy = {};
-            primaryKey = dataList[i][primaryIdName];
-        	if(angular.isDefined(tree.objs[primaryKey])){
-        		// Some forward declaration occurred, so copy the existing data
-        		itemProxy = tree.objs[primaryKey];
-        	}
-            itemProxy.data = dataList[i++];
-            itemProxy.id = itemProxy.data.id;
-            itemProxy.parentId = itemProxy.data.parentId;
-            itemProxy.title = itemProxy.data.title;
-            itemProxy.description = itemProxy.data.description;
-            primaryKey = itemProxy.data[primaryIdName];
-            tree.objs[primaryKey] = itemProxy;
-            parentId = itemProxy.data[parentIdName];
-
-            if (parentId) {
-            	if(angular.isDefined(tree.objs[parentId])){
-                    parent = tree.objs[parentId];
-            	} else {
-            		// Create the parent before it is found
-            		parent = {};
-            		tree.objs[parentId] = parent;
-            	}
-
-            	if (parent.children) {
-                    parent.children.push(itemProxy);
-                } else {
-                    parent.children = [itemProxy];
-                }
-            	// TBD: itemProxy.parentRef = parent;
-            } else {
-                rootIds.push(primaryKey);
-            }
-        }
-
-        for (var i = 0; i < rootIds.length; i++) {
-            tree.data.push(tree.objs[rootIds[i]]);
-        };
-
-        return tree;
-    }
-
-      $scope.$on('currentItemUpdate', function(event, data){
-          console.log(event);
-          console.log(data.id);
-          $location.hash(data.id);
-          $anchorScroll();
-        }
-      )
     $scope.syncLocation = function(){
         $anchorScroll();
     }
-      
-    $scope.changeVisibility = function(){
-      console.log(this);
-      $scope.collapsed[this.itemProxy.item.id] = !$scope.collapsed[this.itemProxy.item.id];
-      var isNowCollapsed = $scope.collapsed[this.itemProxy.item.id];
+
+    $scope.expandAll = function(){
+      // Set all nodes to expanded
+      for(id in $scope.collapsed){
+        $scope.collapsed[id] = false;
+      }
+      // Set all nodes to visible
+      for(id in $scope.hidden){
+        $scope.hidden[id] = false;
+      }
+    }
+    
+    $scope.collapseAll = function(){
+      // Collapse all root nodes that are current expanded
+      for(var idx=0; idx < $scope.newTree.roots.length; idx++){
+        var rootNode = $scope.newTree.roots[idx];
+        if (!$scope.collapsed[rootNode.item.id]){
+          changeVisibilityOn(rootNode);
+        }
+      }        
+
+    }
+    
+    function changeVisibilityOn (proxy){
+      $scope.collapsed[proxy.item.id] = !$scope.collapsed[proxy.item.id];
+      var isNowCollapsed = $scope.collapsed[proxy.item.id];
       var childIdStack = [];
       var childId = "";
-      var proxy = this.itemProxy;
 
       // Add immediate descendants to stack
       for(var idx=0; idx < proxy.children.length; idx++){
@@ -172,9 +85,11 @@ angular
           }        
         }
         $scope.hidden[childId] = isNowCollapsed;
-      }
-
+      }    
+    }
     
+    $scope.changeVisibility = function(){
+      changeVisibilityOn(this.itemProxy);
     }
 
   }]);
@@ -186,7 +101,6 @@ angular
 
     $scope.listTitle = "Children"
     $scope.editedItem = new Item;
-    $scope.itemProxy = {};
     $scope.enableEdit = false;
 
     $scope.newTree = ItemRepository.internalTree;
@@ -194,16 +108,6 @@ angular
     if (angular.isDefined($state.params.parentId)){
     	$scope.editedItem.parentId = $state.params.parentId;
     	$scope.enableEdit=true;
-    }
-
-    $scope.items = [];
-
-    function getChildren() {
-      Item.children($scope.editedItem)
-        .$promise
-        .then(function(results) {
-          $scope.items = results;
-        });
     }
 
 	  function getItem() {
@@ -251,7 +155,7 @@ angular
           .deleteById(item)
           .$promise
           .then(function() {
-            getChildren();
+            console.log("*** TBD:  Need to confirm removal logic")
           });
       };
 
