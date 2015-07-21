d052855fd1193548e11c7a0df5b8d53f326372ad
Josh <jephillips@cohesionforce.com>
2015-07-21 14:27:26 -0500

    Removed old router code

--- kohese-portal/client/js/app.js
+++ kohese-portal/client/js/app.js
@@ -15,46 +15,7 @@ angular
     'mc.resizer'
   ])
   .controller('KoheseController',['$router', KoheseController])
-  //.config(['$stateProvider', '$urlRouterProvider', function($stateProvider,
-  //    $urlRouterProvider) {
-  //
-  //  $stateProvider
-  //    .state('items', {
-  //      url: '/',
-  //      views: {
-  //      	"list@": {
-  //              templateUrl: 'js/item/templates/itemTree.html',
-  //              controller: 'ItemController'
-  //      	}
-  //      }
-  //    });
-  //  $stateProvider
-  //    .state('newItem', {
-  //      parent: 'items',
-  //  	url: '/newItem?parentId',
-  //      views: {
-  //      	"detail@": {
-  //              templateUrl: 'js/item/templates/itemDetail.html',
-  //              controller: 'ItemEditController'
-  //      	}
-  //      }
-  //    });
-  //  $stateProvider
-  //    .state('editItem', {
-  //      parent: 'items',
-  //      url: '/editItem',
-  //      views: {
-  //      	"detail@": {
-  //              templateUrl: 'js/item/templates/itemDetail.html',
-  //              controller: 'ItemEditController'
-  //      	}
-  //      }
-  //    });
-  //
-  //  $urlRouterProvider
-  //    .otherwise('/');
-  //}])
-    .filter('highlight', function($sce) {
+  .filter('highlight', function($sce) {
     return function(text, phrase) {
       if(text !== null){
           if (phrase) text = text.replace(new RegExp('('+phrase+')', 'gi'),
