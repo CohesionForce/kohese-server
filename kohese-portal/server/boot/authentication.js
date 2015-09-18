c6c0d81b708c33423f4a9e6e8c1fab211f47d1b5
dphillips <dphillips@CohesionForce.com>
2015-09-18 01:25:05 -0500

    Added auto-creation of admin account.

--- kohese-portal/server/boot/authentication.js
+++ kohese-portal/server/boot/authentication.js
@@ -1,4 +1,29 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+  
+  server.once('started', function(baseUrl) {
+    var KohesePrincipal = server.models.KohesePrincipal;
+    
+    KohesePrincipal.find({where: {
+        name: 'admin'
+      }
+    }, function(err, principal) {
+      if(principal.length === 0){
+        console.log("::: Creating admin account");
+        KohesePrincipal.upsert({
+           name: 'admin',
+           password: "kohese",
+           createdBy: 'admin',
+           createdOn: Date.now()
+          }, function(err, principal){
+            console.log("::: Created admin account");
+          });
+        } else {
+          console.log("::: Admin account already exists");
+        };
+    });
+    
+  });
+
 };
