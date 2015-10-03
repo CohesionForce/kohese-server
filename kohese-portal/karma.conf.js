b2b7380e3947f09f85578daadc98508bd67167c4
J Langley <jlangley@cohesionforce.com>
2015-10-03 18:39:54 -0500

    Updated to add cobertura for reporting with Hudson

--- kohese-portal/karma.conf.js
+++ kohese-portal/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
 				'bower_components/angular-resource/angular-resource.js',
 				'bower_components/jquery/dist/jquery.js',
 				'client/bundle.js',
-				'tests/unit/components/login-spec.js'],
+				'tests/unit/components/*-spec.js'],
 
 		// list of files to exclude
 		exclude : [],
@@ -25,12 +25,17 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors:
 		// https://npmjs.org/browse/keyword/karma-preprocessor
-		preprocessors : {},
+		preprocessors: {
+		      // source files, that you wanna generate coverage for
+		      // do not include tests or libraries
+		      // (these files will be instrumented by Istanbul)
+		      'client/bundle.js': ['coverage']
+		    },
 
-		// test results reporter to use
+		    // test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters : ['dots', 'junit'],
+		reporters : ['dots', 'junit', 'coverage'],
 		
 		junitReporter : {
 			outputDir: '',
@@ -38,6 +43,23 @@ module.exports = function(config) {
 			suite: '' // suite will become the package name attribute in xml testsuite element
 		},
 
+		coverageReporter: {
+		      // specify a common output directory
+		      dir: 'coverage/',
+		      reporters: [
+		        // reporters not supporting the `file` property
+		        { type: 'html', subdir: 'report-html' },
+		        { type: 'lcov', subdir: 'report-lcov' },
+		        // reporters supporting the `file` property, use `subdir` to directly
+		        // output them in the `dir` directory
+		        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+		        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+		        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+		        { type: 'text', subdir: '.', file: 'text.txt' },
+		        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+		      ]
+		},
+		    
 		// web server port
 		port : 9876,
 
