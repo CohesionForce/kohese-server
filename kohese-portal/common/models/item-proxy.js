a9c9d7ed5a83a5e2004112699aead77155abc653
dphillips <dphillips@CohesionForce.com>
2015-10-27 13:07:59 -0500

    Added sorting by name for children.

--- kohese-portal/common/models/item-proxy.js
+++ kohese-portal/common/models/item-proxy.js
@@ -7,26 +7,27 @@ var _ = require('underscore');
 var tree = {};
 tree.proxyMap = {};
 
-tree.root = new ItemProxy("Internal",{
-  id: "ROOT", 
-  name: "Root of Knowledge Tree"  
+tree.root = new ItemProxy("Internal", {
+  id: "ROOT",
+  name : "Root of Knowledge Tree"
 });
 
-tree.lostAndFound = new ItemProxy("Internal",{
-  id: "LOST+FOUND",
-  name: "Lost-And-Found",
-  description: "Collection of node(s) that are no longer connected."  
+tree.lostAndFound = new ItemProxy("Internal", {
+  id : "LOST+FOUND",
+  name : "Lost-And-Found",
+  description : "Collection of node(s) that are no longer connected."
 });
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 // Create ItemProxy from an existing Item
-//////////////////////////////////////////////////////////////////////////
-function ItemProxy(kind, forItem){
+// ////////////////////////////////////////////////////////////////////////
+function ItemProxy(kind, forItem) {
   var itemId = forItem.id;
-  
+
   var proxy = tree.proxyMap[itemId];
   if (!proxy) {
-//  console.log("::: IP:  Creating " + forItem.id + " - " + forItem.name + " - " + kind);
+    // console.log("::: IP: Creating " + forItem.id + " - " + forItem.name + " -
+    // " + kind);
     proxy = this;
     proxy.children = [];
     tree.proxyMap[itemId] = proxy;
@@ -35,28 +36,28 @@ function ItemProxy(kind, forItem){
   proxy.kind = kind;
   proxy.item = forItem;
 
-  if (kind === "Internal"){
+  if (kind === "Internal") {
     // Don't continue
     return proxy;
   };
-  
+
   var parentId = proxy.item.parentId || "ROOT";
 
   var parent = tree.proxyMap[parentId];
-  
-  if (!parent){
+
+  if (!parent) {
     // Create the parent before it is found
-    parent = createMissingProxy(parentId);    
+    parent = createMissingProxy(parentId);
   }
-  
+
   parent.addChild(proxy);
-  
+
   return proxy;
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 function createMissingProxy(forId) {
   var lostProxy = new ItemProxy("Internal-Lost", {
     id : forId,
@@ -64,148 +65,161 @@ function createMissingProxy(forId) {
     description : "Found children nodes referencing this node as a parent.",
     parentId : "LOST+FOUND"
   });
-  
+
   return lostProxy;
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
-ItemProxy.prototype.addChild = function (childProxy){
-  if (childProxy.parentProxy == this){
-//    console.log("::: IP:  Child " + childProxy.item.name + " already associated with " + this.item.name);
+// ////////////////////////////////////////////////////////////////////////
+ItemProxy.prototype.addChild = function(childProxy) {
+  if (childProxy.parentProxy == this) {
+    // console.log("::: IP: Child " + childProxy.item.name + " already
+    // associated with " + this.item.name);
     return;
   }
-//  console.log("::: IP:  Adding child " + childProxy.item.name + " to " + this.item.name);
+  // console.log("::: IP: Adding child " + childProxy.item.name + " to " +
+  // this.item.name);
   // Determine if this node is already attached to another parent
-  if (childProxy.parentProxy){
+  if (childProxy.parentProxy) {
     childProxy.parentProxy.removeChild(childProxy);
   }
 
-  this.children.push(childProxy);
+  // Determine where to insert the new child
+  var insertAt = this.children.length;  
+  for (var childIdx in this.children){
+    if (childProxy.item.name < this.children[childIdx].item.name){
+      insertAt = childIdx;
+      break;
+    }
+  }
+
+  this.children.splice(insertAt, 0, childProxy);
   childProxy.parentProxy = this;
-  
-  if (this == tree.lostAndFound && tree.lostAndFound.children.length === 1){
-    tree.root.addChild(tree.lostAndFound);    
+
+  if (this == tree.lostAndFound && tree.lostAndFound.children.length === 1) {
+    tree.root.addChild(tree.lostAndFound);
   }
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
-ItemProxy.prototype.removeChild = function (proxy){
-//  console.log("::: IP:  Removing child " + proxy.item.name + " from " + this.item.name);
-  this.children = _.reject(this.children,
-      function(childProxy) {
+// ////////////////////////////////////////////////////////////////////////
+ItemProxy.prototype.removeChild = function(proxy) {
+  // console.log("::: IP: Removing child " + proxy.item.name + " from " +
+  // this.item.name);
+  this.children = _.reject(this.children, function(childProxy) {
     return childProxy.item.id === proxy.item.id;
-  });;
+  });
 
   delete proxy.parentProxy;
-  
-  if (this == tree.lostAndFound && tree.lostAndFound.children.length === 0){
-    tree.root.removeChild(tree.lostAndFound);    
+
+  if (this == tree.lostAndFound && tree.lostAndFound.children.length === 0) {
+    tree.root.removeChild(tree.lostAndFound);
   }
 
-  if (this.kind === "Internal-Lost" && this.children.length === 0){
+  if (this.kind === "Internal-Lost" && this.children.length === 0) {
     this.deleteItem();
   }
 
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
-ItemProxy.getProxyFor = function (id){
-  
-//  console.log("::: IP:  Getting proxy for " + id);
+// ////////////////////////////////////////////////////////////////////////
+ItemProxy.getProxyFor = function(id) {
+
+  // console.log("::: IP: Getting proxy for " + id);
   return tree.proxyMap[id];
-  
+
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
-ItemProxy.dumpAllProxies = function () {
-  for(var proxyId in tree.proxyMap){
+// ////////////////////////////////////////////////////////////////////////
+ItemProxy.dumpAllProxies = function() {
+  for ( var proxyId in tree.proxyMap) {
     var proxy = tree.proxyMap[proxyId];
-    console.log("::: Dumping " + proxy.item.id + " - " + proxy.item.name + " - " + proxy.kind);
+    console.log("::: Dumping " + proxy.item.id + " - " + proxy.item.name
+        + " - " + proxy.kind);
   }
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
-ItemProxy.getRootProxy = function () {
-//  console.log("::: IP: Getting Root Proxy");
+// ////////////////////////////////////////////////////////////////////////
+ItemProxy.getRootProxy = function() {
+  // console.log("::: IP: Getting Root Proxy");
   return tree.root;
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
-ItemProxy.prototype.getChildByName = function (name) {
+// ////////////////////////////////////////////////////////////////////////
+ItemProxy.prototype.getChildByName = function(name) {
   for ( var childIdx in this.children) {
     var child = this.children[childIdx];
     if (child.item.name === name) {
       return child;
     }
   }
-  return null;  
+  return null;
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 ItemProxy.prototype.getDecendents = function() {
   var proxyStack = [];
-  for(var childIdx = this.children.length - 1; childIdx > -1; childIdx--){
-      proxyStack.push(this.children[childIdx]);
+  for (var childIdx = this.children.length - 1; childIdx > -1; childIdx--) {
+    proxyStack.push(this.children[childIdx]);
   }
 
   var decendantList = [];
   var decendant = proxyStack.pop()
-  while(decendant){
-      decendantList.push(decendant);
-      for(var childIdx = decendant.children.length - 1; childIdx > -1; childIdx--){
-          proxyStack.push(decendant.children[childIdx]);
-      }
-      decendant = proxyStack.pop();
+  while (decendant) {
+    decendantList.push(decendant);
+    for (var childIdx = decendant.children.length - 1; childIdx > -1; childIdx--) {
+      proxyStack.push(decendant.children[childIdx]);
+    }
+    decendant = proxyStack.pop();
   }
   return decendantList;
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 ItemProxy.prototype.dumpProxy = function(indent) {
   var thisIndent = "";
   var childIndent = "|-";
-  if (indent){
+  if (indent) {
     thisIndent = indent;
     childIndent = "| " + thisIndent;
   }
-  
-  console.log ("=== " + thisIndent + this.item.id + " - " + this.item.name + " - " + this.kind );
-  
-  for(var childIdx in this.children){
+
+  console.log("=== " + thisIndent + this.item.id + " - " + this.item.name
+      + " - " + this.kind);
+
+  for ( var childIdx in this.children) {
     var childProxy = this.children[childIdx];
     childProxy.dumpProxy(childIndent);
   }
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 ItemProxy.prototype.deleteItem = function() {
   var byId = this.item.id;
 
   console.log("::: Deleting proxy for " + byId);
 
-  if (this.parentProxy){
-    this.parentProxy.removeChild(this);    
+  if (this.parentProxy) {
+    this.parentProxy.removeChild(this);
   }
-  
-  if (this.children.length !== 0){
+
+  if (this.children.length !== 0) {
     console.log("::: -> Node still has children");
     createMissingProxy(byId);
   } else {
@@ -215,39 +229,40 @@ ItemProxy.prototype.deleteItem = function() {
 
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 function copyAttributes(fromItem, toItem) {
 
   // Copy attributes proxy
-  for (var key in fromItem) {
-      if (fromItem.hasOwnProperty(key) && (key.charAt(0) !== '$') && !_.isEqual(fromItem[key], toItem[key])) {
-          console.log("!!! Updating " + key);
-          toItem[key] = fromItem[key];
-      }
+  for ( var key in fromItem) {
+    if (fromItem.hasOwnProperty(key) && (key.charAt(0) !== '$')
+        && !_.isEqual(fromItem[key], toItem[key])) {
+      console.log("!!! Updating " + key);
+      toItem[key] = fromItem[key];
+    }
   }
   // Check for unexpected values
-  for (var key in toItem) {
-    if (key !== "__deletedProperty" && toItem.hasOwnProperty(key) && (key.charAt(0) !== '$') && !fromItem.hasOwnProperty(key)) {
+  for ( var key in toItem) {
+    if (key !== "__deletedProperty" && toItem.hasOwnProperty(key)
+        && (key.charAt(0) !== '$') && !fromItem.hasOwnProperty(key)) {
       console.log("!!! Deleted Property: " + key);
-      if (!toItem.__deletedProperty){
+      if (!toItem.__deletedProperty) {
         toItem.__deletedProperty = {};
       }
       toItem.__deletedProperty[key] = toItem[key];
       delete toItem[key];
-    }    
+    }
   }
 }
 
-
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
-ItemProxy.prototype.updateItem = function (modelKind, withItem) {
+// ////////////////////////////////////////////////////////////////////////
+ItemProxy.prototype.updateItem = function(modelKind, withItem) {
   console.log("!!! Updating " + modelKind + " - " + this.item.id);
 
-      // Determine if item kind changed
+  // Determine if item kind changed
   var newKind = modelKind;
 
   if (newKind !== this.kind) {
@@ -265,9 +280,11 @@ ItemProxy.prototype.updateItem = function (modelKind, withItem) {
   }
 
   var newParentId = withItem.parentId;
-  console.log("::: Eval Parent Id old: " + oldParentId + " new: " + newParentId)
+  console
+      .log("::: Eval Parent Id old: " + oldParentId + " new: " + newParentId)
   if (oldParentId !== newParentId) {
-    console.log("::: Parent Id changed from " + oldParentId + " to " + newParentId)
+    console.log("::: Parent Id changed from " + oldParentId + " to "
+        + newParentId)
 
     var newParentProxy;
     if (newParentId === "") {
@@ -279,25 +296,25 @@ ItemProxy.prototype.updateItem = function (modelKind, withItem) {
     if (!newParentProxy) {
       newParentProxy = createMissingProxy(newParentId);
     }
-    
+
     newParentProxy.addChild(this);
   }
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 ItemProxy.getAllItemProxies = function() {
   var itemProxyList = [];
-  for (var key in tree.proxyMap) {
-      itemProxyList.push(tree.proxyMap[key]);
+  for ( var key in tree.proxyMap) {
+    itemProxyList.push(tree.proxyMap[key]);
   }
   return itemProxyList;
 }
 
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 //
-//////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////
 module.exports = ItemProxy;
 module.exports.getRootProxy = ItemProxy.getRootProxy;
 module.exports.getProxyFor = ItemProxy.getProxyFor;
