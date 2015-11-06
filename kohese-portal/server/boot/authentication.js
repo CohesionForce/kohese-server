ba7498d4d614f11d54d19d55bdedbcec8cb0d187
dphillips <dphillips@CohesionForce.com>
2015-11-06 09:12:31 -0600

    Added check for Users item.

--- kohese-portal/server/boot/authentication.js
+++ kohese-portal/server/boot/authentication.js
@@ -1,8 +1,38 @@
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
+  var UsersItem;
   
-  server.once('started', function(baseUrl) {
+  function checkAndCreateUsersItem() {
+    var Item = server.models.Item;
+    
+    Item.find({where: {
+        name: 'Users'
+      }
+    }, function(err, principal) {
+      if(principal.length === 0){
+        console.log("::: Creating Users Item");
+        var now = Date.now();
+        Item.create({
+           name: 'Users',
+           description: 'User accounts',
+           createdBy: 'admin',
+           createdOn: now,
+           modifiedBy: 'admin',
+           modifiedOn: now
+          }, function(err, item){
+            console.log(">>> " + err);
+            console.log("::: Created Users Item");
+            console.log(JSON.stringify(item,null,"  "));
+          });
+        } else {
+          console.log("::: Users Item already exists");
+          console.log(JSON.stringify(principal[0],null,"  "));
+        };
+    });    
+  }
+  
+  function checkAndCreateAdminAccount() {
     var KoheseUser = server.models.KoheseUser;
     
     KoheseUser.find({where: {
@@ -26,8 +56,12 @@ module.exports = function enableAuthentication(server) {
         } else {
           console.log("::: Admin account already exists");
         };
-    });
-    
+    });    
+  }
+  
+  server.once('started', function(baseUrl) {
+    checkAndCreateUsersItem();
+    checkAndCreateAdminAccount();
   });
 
 };
