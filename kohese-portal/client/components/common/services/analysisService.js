01b0bdb9ec593ca8d437172a5b57b514575daeca
dphillips <dphillips@CohesionForce.com>
2016-01-25 16:06:19 -0600

    Add Noun and Verb token filter.

--- kohese-portal/client/components/common/services/analysisService.js
+++ kohese-portal/client/components/common/services/analysisService.js
@@ -14,7 +14,9 @@ function AnalysisService(Analysis, ItemRepository) {
         "No Filter": ["XKPC","RRC","X","NAC","WHPP","QP","LST","WHNP","PRT","INTJ","WHAVP","PRN","FRAG","WHADJP","PP","CONJP","VP","NX","ADVP","UCP","NP","ADJP","WRB","VB","PRP","JJS","WPS","UH","POS","JJR","WP","TO","PDT","JJ","WDT","SYM","NNPS","IN","VBZ","RP","NNP","FW","VBP","RBS","NNS","EX","VBN","RBR","NN","DT","VBG","RB","MD","CD","VBD","PRPS","LS","CC"],
         "Pseudo": ["XKPC"],
         "Noun Phrases": ["NP"],
-        "Verb Phrases": ["VP"]
+        "Verb Phrases": ["VP"],
+        "Noun": ["NX","NP","NNPS","NNP","NNS","NN","PRPS","WP","WPS","PRP","WHNP"],
+        "Verb": ["VP","VB","VBZ","VBP","VBN","VBG","VBD"]
     }
 
     service.filterPOS = function (summary, filterCriteria){
