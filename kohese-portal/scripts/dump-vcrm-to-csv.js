1c8084ac23c82d45988aaf87185021fda1692729
dphillips <dphillips@CohesionForce.com>
2016-02-12 23:25:12 -0600

    Updated vcrm dump to simplify the output.

--- kohese-portal/scripts/dump-vcrm-to-csv.js
+++ kohese-portal/scripts/dump-vcrm-to-csv.js
@@ -3,7 +3,7 @@ console.log(process.argv);
 
 var KDB = require('../server/kdb.js');
 var parseHeaderName=/(([0-9]*\.*)*)\s*(.*)/;
-var isDescriptionSentence=/^description\-Sentence/;
+var isDescriptionSentence=/^description\-Sentence-(.*)$/;
 
 var forItemId="db13bfc0-ae41-11e5-af49-9b039241499e";
 
@@ -23,16 +23,12 @@ console.log("::: Found proxy: " + document.item.name);
 console.log("KUID,Name,Sentence ID, Text");
 var displayItem = function(proxy){
   var onId = proxy.item.id;
-  var itemPrefix = proxy.item.id + ",|" + proxy.item.name;
-  var fullItem = itemPrefix + "|,|" + "Item" + "|,|" + proxy.item.description + "|";
+  var fullItem = proxy.item.id + ",|" + proxy.item.name + "|,|" + "Item" + "|,|" + proxy.item.description + "|";
 
   var nameSplit = proxy.item.name.match(parseHeaderName);
-  console.log("::: >> " + nameSplit);
-  console.log("::: }} " + nameSplit[1] + "|" + nameSplit[3]);
   var itemNum = nameSplit[1];
   var itemNameText = nameSplit[3];
-  var itemPrefix = proxy.item.id + ",|" + itemNum;
-  console.log(itemPrefix + "|,|" + "Heading" + "|,|"  + itemNameText + "|");
+  console.log(proxy.item.id + ":N,|" + itemNum + "|,|" + "Heading" + "|,|"  + itemNameText + "|");
   console.log(fullItem);
 
   var analysis = KDB.retrieveModelInstance("Analysis", onId);
@@ -43,7 +39,8 @@ var displayItem = function(proxy){
   for(var listIdx in analysis.list){
     var listItem = analysis.list[listIdx];
     if(listItem.displayType === 'Sentence' && isDescriptionSentence.test(listItem.displayId)){
-      console.log(itemPrefix + "|,|" + listItem.displayId + "|,|" + listItem.text + "|");
+      var displayIdSplit = listItem.displayId.match(isDescriptionSentence);
+      console.log(proxy.item.id + ":D" + displayIdSplit[1] + ",|" + itemNum + "|,|" + "Description:" + displayIdSplit[1] + "|,|" + listItem.text + "|");
 //      console.log(listItem);
     }
   }
