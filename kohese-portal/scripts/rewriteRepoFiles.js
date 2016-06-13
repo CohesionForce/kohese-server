02e257e03c5ff388551fa76f22db7d036bbccdde
dphillips <dphillips@CohesionForce.com>
2016-06-13 13:48:13 -0500

    Added additional logging to support determination of progress.

--- kohese-portal/scripts/rewriteRepoFiles.js
+++ kohese-portal/scripts/rewriteRepoFiles.js
@@ -3,10 +3,12 @@ var kdb = require('../server/kdb.js');
 var ItemProxy = kdb.ItemProxy;
  
 var root = ItemProxy.getRootProxy();
+//var root = ItemProxy.getProxyFor("80b4be90-0f99-11e5-9d90-27fe940861ba");
  
 var descendants = root.getDescendants();
+var descendantCount = descendants.length;
 for(var idx in descendants){
   var proxy = descendants[idx];
-  console.log("--> rewrite: " + proxy.kind + " - " + proxy.item.id + " - " + proxy.item.name);
+  console.log("--> rewrite[" + idx + "/" + descendantCount + "]: " + proxy.kind + " - " + proxy.item.id + " - " + proxy.item.name);
   kdb.storeModelInstance(proxy.kind, proxy.item);
 }
