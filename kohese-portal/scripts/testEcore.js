edba8a82d249c9169f3a0810a6e05ef90180f054
aschneider <aschneider@cohesionforce.com>
2016-07-22 12:18:04 -0500

    Added support for processing more than two files at a time.

--- kohese-portal/scripts/testEcore.js
+++ kohese-portal/scripts/testEcore.js
@@ -1,29 +1,6 @@
-/**
- * 
- */
-
-fs = require('fs');
-util = require('util');
-var Ecore = require('ecore/dist/ecore.xmi');
-
-console.log(process.argv);
-
-var forModelFile = '';
-var forInstanceFile;
-
-if(process.argv[2]){
-  forModelFile=process.argv[2];
-} else {
-  forModelFile='../kohese-design/model/Kohese.ecore';  
-  console.log('!!! Should supply ecore/xmi file to be dumped.');
-}
-
-if (process.argv[3]) {
-  forInstanceFile = process.argv[3];
-}
-
-console.log("::: Testing Ecore.  Using: " + forModelFile);
-
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 function displayModelInfo(model) {
   var ePackage = model.get('contents').first();
 
@@ -37,54 +14,46 @@ function displayModelInfo(model) {
   }));  
 }
 
-var callback = function(model, err) {
-  if (err) {
-      console.log('*** Failed loading model: ', err);
-      return;
-  }
-
-  displayModelInfo(model);
-};
-
-// Processing Asynchronously
-//fs.readFile('../kohese-design/model/KoheseTypes.ecore', 'utf8', function (err,data) {
-//  if (err) return console.log(err);
+//////////////////////////////////////////////////////////////////////////
 //
-//  Ecore.Resource.create({ uri: '../kohese-design/model/KoheseTypes.ecore' }).load(data, callback, {format: Ecore.XMI});
-//});
+//////////////////////////////////////////////////////////////////////////
+function processFile(file) {
+	var resourceSet = Ecore.ResourceSet.create();
+	var resource = resourceSet.create({uri : './testFile.kfile'});
 
-// Processing Synchronously
-var resourceSet = Ecore.ResourceSet.create();
-var resource = resourceSet.create({uri : './testFile.kfile'});
+	var fileContents = fs.readFileSync(file, 'utf8');
+	try { 
+		resource.parse(fileContents, Ecore.XMI);
+	} catch(err) {
+		console.log('*** Failed parsing file: ' + file);
+		console.trace(err);
+	    return;
+	}
+	console.log("::: Sync Version");
 
-var kModelFile = fs.readFileSync(forModelFile, 'utf8');
-resource.parse(kModelFile, Ecore.XMI);
-console.log("::: Sync Version");
-//var ePackage2 = resource.get('contents').first();
-//console.log('loaded ePackage2', ePackage2.get('name'));
-displayModelInfo(resource);
-
-var result = resource.to(Ecore.JSON);
-console.log("::: JSON Dump of " + forModelFile);
-console.log(util.inspect(result, false, null));
-
-if (forInstanceFile) {
-	console.log('::: Processing instance:  ' + forInstanceFile);
+	var firstElement = resource.get('contents').first();
+	if(firstElement.eClass.values.name === 'EPackage') {
+		console.log("::: Adding to registry: " + firstElement.get('name'));
+		Ecore.EPackage.Registry.register(firstElement);
+		displayModelInfo(resource);
+	}
 	
-	var kPackage = resource.get('contents').first();
-	Ecore.EPackage.Registry.register(kPackage);
-	
-	var instanceRS = Ecore.ResourceSet.create();
-	var instanceResource = instanceRS.create({uri : './testFile.kifile'});
-	var kInstanceFile = fs.readFileSync(forInstanceFile, 'utf8');
-	instanceResource.load(kInstanceFile, function(instance, err){
-		if (err) {
-		      console.log('*** Failed loading model instance: ', err);
-		      return;
-		  }
-		
-		var iResult = instance.to(Ecore.JSON);
-		console.log("::: JSON Dump of " + forInstanceFile);
-		console.log(util.inspect(iResult, false, null));
-	}, {format: Ecore.XMI});
+	var result = resource.to(Ecore.JSON);
+	console.log("::: JSON Dump of " + file);
+	console.log(util.inspect(result, false, null));
+}
+
+//////////////////////////////////////////////////////////////////////////
+//  Main Processing
+//////////////////////////////////////////////////////////////////////////
+
+fs = require('fs');
+util = require('util');
+var Ecore = require('ecore/dist/ecore.xmi');
+
+console.log(process.argv.length);
+for(var argidx = 2; argidx < process.argv.length; argidx++) {
+	var fileName = process.argv[argidx];
+	console.log('::: Processing ' + fileName);
+	processFile(fileName);
 }
