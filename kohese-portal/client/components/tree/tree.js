2ca0213d6c5385bfc52852ac59b7aecf3f1b0fb0
Josh P <jephillips@johnny12.cfi.internal>
2017-10-30 09:28:46 -0500

    Delete all children from client side

--- kohese-portal/client/components/tree/tree.js
+++ kohese-portal/client/components/tree/tree.js
@@ -254,6 +254,13 @@ function KTreeController(ItemRepository, ActionService, UserService, $timeout, $
 
     treeCtrl.removeItem = function (proxy) {
         var itemId = proxy.item.id;
+        var modalDefaults = {};
+
+        if (proxy.children.length > 0)
+        {
+            modalDefaults.templateUrl = 'components/common/services/modal-service/deleteModal.html'
+        }
+
         var modalOptions = {
             closeButtonText : 'Cancel',
             actionButtonText : 'Delete',
@@ -261,10 +268,10 @@ function KTreeController(ItemRepository, ActionService, UserService, $timeout, $
             bodyText: 'Are you sure you want to delete this item?'
         }
 
-        ModalService.showModal({}, modalOptions).then((result)=>
+        ModalService.showModal(modalDefaults, modalOptions).then((result)=>
             {
             ItemRepository
-            .deleteItem(proxy).then(function () 
+            .deleteItem(proxy, result.deleteChildren).then(function () 
                 {
                 console.log("::: Item has been deleted: " + itemId);
                 });
