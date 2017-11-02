014e23b7316348cfc287438dd3d7f2358be5d65a
Josh P <jephillips@johnny12.cfi.internal>
2017-11-02 13:43:12 -0500

    Fixed some scrolling issues in terms and added first pass of chunk template

--- kohese-portal/client/components/common/directives/analysis-directives/chunk-view.js
+++ kohese-portal/client/components/common/directives/analysis-directives/chunk-view.js
@@ -1,4 +1,4 @@
-function ChunkViewController($scope, tabService){
+function ChunkViewController($scope, $timeout, tabService, analysisService){
     const ctrl = this;
     var currentTab = tabService.getCurrentTab();
 
@@ -7,11 +7,57 @@ function ChunkViewController($scope, tabService){
     
     if(!controllerRestored) {
         // Initialization Block
+        ctrl.itemProxy = $scope.itemProxy;
+    
+        ctrl.analysisChunkLimit = 100;
+        ctrl.analysisSumarrySortField = ['text', '-count'];
+
+        ctrl.analysisFilterPOS = analysisService.filterPOS;
+        ctrl.analysisPOSFilterCriteria = analysisService.posFilterCriteria;
+        ctrl.analysisPOSFilterCriteriaList = Object.keys(analysisService.posFilterCriteria);
+        ctrl.analysisPOSFilterName = "Standard";
+
+        ctrl.filterString = "";
+        ctrl.filterTextTimeout;
+        ctrl.filterList = []
+
+        ctrl.analysisFilterString = "";
+        ctrl.analysisFilterRegex = null;
     }
 
     $scope.$on('tabSelected', function () {
         tabService.bundleController(ctrl, 'ChunkViewController', currentTab.id);
     });
+
+    ctrl.fetchAnalysis = function () {
+        analysisService.fetchAnalysis(ctrl.itemProxy).then(function (results){
+          $scope.$apply();
+          console.log(ctrl.itemProxy);
+        });
+      };
+
+    ctrl.filterChunks = function(summary) {
+    var MatchesStringFilter;
+    var MatchesPOS = ctrl.analysisFilterPOS(summary,
+            ctrl.analysisPOSFilterCriteria[ctrl.analysisPOSFilterName]) 
+    if (MatchesPOS){
+        MatchesStringFilter = 
+            ((ctrl.analysisFilterRegex === null) 
+            || ctrl.analysisFilterRegex.test(summary.text));
+    }
+    
+        return (MatchesPOS && MatchesStringFilter)
+    };
+
+    ctrl.submitSummaryFilter = function(text){
+        console.log(ctrl.itemProxy);
+    }
+
+    ctrl.getChunkCount = function() {
+        return 5;
+    }
+
+    ctrl.fetchAnalysis();
 }
 
 function ChunkViewDirective(){
