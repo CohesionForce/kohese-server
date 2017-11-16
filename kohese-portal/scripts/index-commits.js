89e354157248482a61b180058a59cb206878a8e0
David R. Phillips <dphillips@CohesionForce.com>
2017-11-16 05:10:28 -0600

    Refactored cache directory and blob loading.

--- kohese-portal/scripts/index-commits.js
+++ kohese-portal/scripts/index-commits.js
@@ -6,38 +6,72 @@ var kdbFS = require('../server/kdb-fs.js');
 var _ = require('underscore');
 
 const INDEX_DIRECTORY = path.join('kdb', 'index');
-const OBJECT_DIRECTORY = 'kdb/cache/oid';
+
+const CACHE_DIRECTORY = path.join('kdb', 'cache');
+const OBJECT_DIRECTORY = path.join(CACHE_DIRECTORY, 'git-object');
+const COMMIT_DIRECTORY = path.join(OBJECT_DIRECTORY, 'commit');
+const BLOB_DIRECTORY = path.join(OBJECT_DIRECTORY, 'blob');
+const TREE_DIRECTORY = path.join(OBJECT_DIRECTORY, 'tree');
 
 const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-const repository = process.argv[2];
+const repositoryPath = process.argv[2];
 
-kdbFS.createDirIfMissing('kdb/cache');
-kdbFS.createDirIfMissing('kdb/cache/oid');
+// TODO Remove or Replace Index Dir with Cache Dir
+kdbFS.createDirIfMissing(INDEX_DIRECTORY);
+
+kdbFS.createDirIfMissing(CACHE_DIRECTORY);
+kdbFS.createDirIfMissing(OBJECT_DIRECTORY);
+kdbFS.createDirIfMissing(COMMIT_DIRECTORY);
+kdbFS.createDirIfMissing(BLOB_DIRECTORY);
+kdbFS.createDirIfMissing(TREE_DIRECTORY);
 
 var cachedOIDs = [];
+var repoBlobs = {};
 
-var repoContents = {};
-
-function loadCachedObjects(repoContents) {
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+function loadCachedObjects(repoBlobs) {
   console.log('::: Loading cached objects');
-  var oidFiles = kdbFS.getRepositoryFileList('kdb/cache/oid', /\.json$/);
+  var oidFiles = kdbFS.getRepositoryFileList(BLOB_DIRECTORY, /\.json$/);
 
   oidFiles.forEach((oidFile) => {
-//    console.log('::: Loading ' + oidFile);
     var oid = oidFile.replace(/\.json$/, '');
-    var object = kdbFS.loadJSONDoc('kdb/cache/oid/' + oidFile);
-    repoContents[oid]=object;
+    var object = kdbFS.loadJSONDoc(BLOB_DIRECTORY + '/' + oidFile);
+    repoBlobs[oid]=object;
   });
   
-  cachedOIDs = Object.keys(repoContents).sort();
+  cachedOIDs = Object.keys(repoBlobs).sort();
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+function storeCachedObjects() {
+  var updatedOIDs = Object.keys(repoBlobs).sort();
+  var newOIDs = _.difference(updatedOIDs, cachedOIDs);
+  console.log('New Objects Found: ' + newOIDs.length);
+  
+  newOIDs.forEach((oid) => {
+    kdbFS.storeJSONDoc(BLOB_DIRECTORY + '/' + oid + '.json', repoBlobs[oid]);
+  });  
+  
+  cachedOIDs = updatedOIDs;
+}
+
+//////////////////////////////////////////////////////////////////////////
+// Main Processing
+//////////////////////////////////////////////////////////////////////////
+
+// Load Cached Objects From Prior Runs
 var beforeTime = Date.now();
-loadCachedObjects(repoContents);
+loadCachedObjects(repoBlobs);
 var afterTime = Date.now();
 var deltaTime = afterTime-beforeTime;
 console.log('--- Load Cached Objects Time: ' + deltaTime/1000);
 
+
+// Determine what has already been indexed
 var commitFileList = kdbFS.getRepositoryFileList(INDEX_DIRECTORY, /\.json$/);
 
 var commitIndexed = {};
@@ -47,19 +81,7 @@ commitFileList.forEach((commitFile) => {
   commitIndexed[commitId] = true;
 });
 
-function storeCachedObjects() {
-  var updatedOIDs = Object.keys(repoContents).sort();
-  var newOIDs = _.difference(updatedOIDs, cachedOIDs);
-  console.log('New Objects Found: ' + newOIDs.length);
-  
-  newOIDs.forEach((oid) => {
-    kdbFS.storeJSONDoc('kdb/cache/oid/' + oid + '.json', repoContents[oid]);
-  });  
-  
-  cachedOIDs = updatedOIDs;
-}
-
-generateCommitHistoryIndices(repository, false).then(() => {
+generateCommitHistoryIndices(repositoryPath, false).then(() => {
   console.log('::: Finished Indexing');
   storeCachedObjects();
 });
@@ -67,6 +89,7 @@ generateCommitHistoryIndices(repository, false).then(() => {
 //////////////////////////////////////////////////////////////////////////
 //
 //////////////////////////////////////////////////////////////////////////
+//TODO overwrite flag does not appear to do anything
 function generateCommitHistoryIndices(p, overwrite) {
   return nodegit.Repository.open(p).then(function (r) {
     return r.getMasterCommit().then(function (mc) {
@@ -86,6 +109,9 @@ function generateCommitHistoryIndices(p, overwrite) {
   });
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 function indexCommit(repository, commits) {
   var commit = commits.shift();
 
@@ -117,12 +143,12 @@ function indexCommit(repository, commits) {
       for (var id in entryMap.objects) {
         (function (idKey) {
           var oid = entryMap.objects[idKey].oid;
-          if (repoContents[oid]){
-            new ItemProxy(entryMap.objects[idKey].kind, repoContents[oid]);
+          if (repoBlobs[oid]){
+            new ItemProxy(entryMap.objects[idKey].kind, repoBlobs[oid]);
           } else {
             promises.push(getContents(repository, entryMap.objects[idKey].oid).then(function (contents) {
-              repoContents[oid] = contents;
-              new ItemProxy(entryMap.objects[idKey].kind, repoContents[oid]);
+              repoBlobs[oid] = contents;
+              new ItemProxy(entryMap.objects[idKey].kind, repoBlobs[oid]);
             }));          
           }
         })(id);
@@ -146,6 +172,9 @@ function indexCommit(repository, commits) {
   }  
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 function getIndexEntries(tree, paths) {
   var p;
   if (paths.length > 0) {
@@ -192,6 +221,9 @@ function getIndexEntries(tree, paths) {
   });
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 function parseTree(tree) {
   var promises = [];
   for (var j = 0; j < tree.entryCount(); j++) {
