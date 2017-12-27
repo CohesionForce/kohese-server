d187e929e80dfc432af45911ad2ea1bb1e7813aa
David R. Phillips <dphillips@CohesionForce.com>
2017-12-27 15:24:20 -0600

    Moved Analysis code to server.  Removed unused Loopback based classes.

--- kohese-portal/common/models/decision.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(Decision) {
-
-  // Remote methods are not inherited
-  Decision.beforeRemote('create', Decision.addModificationHistory);
-  Decision.beforeRemote('upsert', Decision.addModificationHistory);
-
-};
