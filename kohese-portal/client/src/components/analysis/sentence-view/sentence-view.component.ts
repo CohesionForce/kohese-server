0e9e7e172b1eb1545a62bfa6f30e7d3e5c1e7162
Josh P <jephillips@johnny12.cfi.internal>
2018-01-05 10:57:06 -0600

    Add itemproxy inputs : analysis

--- kohese-portal/client/src/components/analysis/sentence-view/sentence-view.component.ts
+++ kohese-portal/client/src/components/analysis/sentence-view/sentence-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Input, Component, OnInit, OnDestroy } from '@angular/core';
 
 import { AnalysisViewComponent } from '../AnalysisViewComponent.class';
 import { NavigatableComponent } from '../../../classes/NavigationComponent.class';
@@ -6,13 +6,31 @@ import { NavigationService } from '../../../services/navigation/navigation.servi
 import { TabService } from '../../../services/tab/tab.service';
 import { AnalysisService } from '../../../services/analysis/analysis.service';
 
+import { ItemProxy } from '../../../../../common/models/item-proxy';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs/Subscription';
+
 @Component({
   selector: 'sentence-view',
   templateUrl : './sentence-view.component.html'
 })
 export class SentenceViewComponent extends AnalysisViewComponent
                                    implements OnInit, OnDestroy {
+ /* UI Switches */
+ loadLimit : number;
+ reverse : boolean;
+ sortField : string;
+ /* Data */
+ @Input()
+ itemProxy : ItemProxy;
 
+ /* Observables */
+ @Input()
+ filterSubject : BehaviorSubject<string>;
+
+ /* Subscriptions */
+ filterSubjectSubscription : Subscription;
   constructor(NavigationService : NavigationService,
               TabService : TabService,
               AnalysisService : AnalysisService) {
--- kohese-portal/client/src/components/analysis/term-view/term-view.component.ts
+++ kohese-portal/client/src/components/analysis/term-view/term-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Input, Component, OnInit, OnDestroy } from '@angular/core';
 
 import { AnalysisViewComponent } from '../AnalysisViewComponent.class';
 import { NavigatableComponent } from '../../../classes/NavigationComponent.class';
@@ -6,6 +6,10 @@ import { NavigationService } from '../../../services/navigation/navigation.servi
 import { TabService } from '../../../services/tab/tab.service';
 import { AnalysisService } from '../../../services/analysis/analysis.service';
 
+import { ItemProxy } from '../../../../../common/models/item-proxy';
+
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'term-view',
@@ -13,7 +17,22 @@ import { AnalysisService } from '../../../services/analysis/analysis.service';
 })
 export class TermViewComponent extends AnalysisViewComponent
                                    implements OnInit, OnDestroy {
-  constructor(NavigationService : NavigationService,
+   /* UI Switches */
+   loadLimit : number;
+   reverse : boolean;
+   sortField : string;
+   /* Data */
+   @Input()
+   itemProxy : ItemProxy;
+
+   /* Observables */
+   @Input()
+   filterSubject : BehaviorSubject<string>;
+
+   /* Subscriptions */
+   filterSubjectSubscription : Subscription;
+
+   constructor(NavigationService : NavigationService,
               TabService : TabService,
               AnalysisService : AnalysisService) {
     super(NavigationService, TabService, AnalysisService);
