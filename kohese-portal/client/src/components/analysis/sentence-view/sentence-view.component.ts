b1116d5ff475728aff467b8764b4314d3a5b2642
Josh P <jephillips@johnny12.cfi.internal>
2018-01-05 13:49:57 -0600

    Sentence and Term port

--- kohese-portal/client/src/components/analysis/sentence-view/sentence-view.component.ts
+++ kohese-portal/client/src/components/analysis/sentence-view/sentence-view.component.ts
@@ -1,15 +1,16 @@
-import { Input, Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
 import { AnalysisViewComponent } from '../AnalysisViewComponent.class';
 import { NavigatableComponent } from '../../../classes/NavigationComponent.class';
 import { NavigationService } from '../../../services/navigation/navigation.service';
 import { TabService } from '../../../services/tab/tab.service';
-import { AnalysisService } from '../../../services/analysis/analysis.service';
 
-import { ItemProxy } from '../../../../../common/models/item-proxy';
-
+import { ItemProxy } from '../../../../../common/models/item-proxy'
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subscription } from 'rxjs/Subscription';
+import { AnalysisService } from '../../../services/analysis/analysis.service';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'sentence-view',
@@ -21,6 +22,9 @@ export class SentenceViewComponent extends AnalysisViewComponent
  loadLimit : number;
  reverse : boolean;
  sortField : string;
+ showSentencesInDetails : boolean;
+ showPhrasesInDetails : boolean;
+ showTokensInDetails : boolean;
  /* Data */
  @Input()
  itemProxy : ItemProxy;
@@ -39,10 +43,46 @@ export class SentenceViewComponent extends AnalysisViewComponent
   }
 
   ngOnInit () {
+    this.showSentencesInDetails = true;
+    this.showPhrasesInDetails = false;
+    this.showTokensInDetails = false;
 
+    this.filterSubjectSubscription = this.filterSubject.subscribe(newFilter => {
+      this.filter = newFilter;
+      this.onFilterChange();
+    })
+
+
+    this.loadLimit = 100;
+    this.sortField = '-count';
+    this.reverse = false;
   }
 
   ngOnDestroy () {
-
+    this.filterSubjectSubscription.unsubscribe();
   }
+
+  getDetailsCount = function () {
+    return $('#theDetailsBody').find('tr').length;
+  };
+
+  newSort (term : string) {
+    if (this.sortField === term) {
+      this.reverse = !this.reverse;
+    } else {
+      this.sortField = term;
+      this.reverse = false;
+    }
+  }
+  filterDetails (listItem) : boolean {
+    return listItem.displayLevel == 1 &&
+                (this.filterRegex === null ||
+                 this.filterRegex.test(listItem.item.name) ||
+                 this.filterRegex.test(listItem.item.description)) ||
+               (listItem.displayLevel == 2 && this.showSentencesInDetails ||
+                listItem.displayLevel == 3 && this.showPhrasesInDetails ||
+                listItem.displayLevel == 4 && this.showTokensInDetails
+               ) &&
+                (this.filterRegex === null || this.filterRegex.test(listItem.text));
+  };
 }
--- kohese-portal/client/src/components/analysis/term-view/term-view.component.ts
+++ kohese-portal/client/src/components/analysis/term-view/term-view.component.ts
@@ -1,16 +1,16 @@
-import { Input, Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
 import { AnalysisViewComponent } from '../AnalysisViewComponent.class';
 import { NavigatableComponent } from '../../../classes/NavigationComponent.class';
 import { NavigationService } from '../../../services/navigation/navigation.service';
 import { TabService } from '../../../services/tab/tab.service';
-import { AnalysisService } from '../../../services/analysis/analysis.service';
 
-import { ItemProxy } from '../../../../../common/models/item-proxy';
-
+import { ItemProxy } from '../../../../../common/models/item-proxy'
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subscription } from 'rxjs/Subscription';
+import { AnalysisService } from '../../../services/analysis/analysis.service';
 
+import * as $ from 'jquery';
 @Component({
   selector: 'term-view',
   templateUrl : './term-view.component.html'
@@ -40,10 +40,32 @@ export class TermViewComponent extends AnalysisViewComponent
   }
 
   ngOnInit () {
+    this.filterSubjectSubscription = this.filterSubject.subscribe(newFilter => {
+      this.filter = newFilter;
+      this.onFilterChange();
+    })
 
+
+    this.loadLimit = 100;
+    this.sortField = '-count';
+    this.reverse = false;
   }
 
   ngOnDestroy () {
-
+    this.filterSubjectSubscription.unsubscribe();
   }
+
+  getTermCount = function () {
+    return $('#theTokensBody').find('tr').length;
+  };
+
+  newSort (term : string) {
+    if (this.sortField === term) {
+      this.reverse = !this.reverse;
+    } else {
+      this.sortField = term;
+      this.reverse = false;
+    }
+  }
+
 }
