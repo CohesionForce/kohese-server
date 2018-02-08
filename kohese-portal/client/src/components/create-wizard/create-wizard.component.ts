bd7f073f621690874fa236cc10c3cf5322f4e0b5
Josh P <jephillips@johnny12.cfi.internal>
2018-02-08 10:39:31 -0600

    Finished remaining schemas. Work towards create wizard container

--- kohese-portal/client/src/components/create-wizard/create-wizard.component.ts
+++ kohese-portal/client/src/components/create-wizard/create-wizard.component.ts
@@ -9,6 +9,7 @@ import { ItemProxy } from '../../../../common/models/item-proxy.js';
 import { ItemRepository } from '../../services/item-repository/item-repository.service';
 import { Subscription } from 'rxjs/Subscription';
 import { ImportService } from '../../services/import/import.service';
+import { DynamicTypesService } from '../../services/dynamic-types/dynamic-types.service';
 
 @Component({
   selector : 'create-wizard',
@@ -19,12 +20,17 @@ export class CreateWizardComponent extends NavigatableComponent
   @Input()
   private itemProxy: ItemProxy;
   private repoStatusSubscription: Subscription;
+  models : Array<ItemProxy>;
   types: Array<ItemProxy>;
+  recentProxies : Array<ItemProxy>;
+  selectedType : ItemProxy;
+  selectedParent : ItemProxy;
 
   constructor(@Optional() @Inject(MAT_DIALOG_DATA) private data: any,
               protected NavigationService : NavigationService,
               private itemRepository: ItemRepository,
-              private ImportService : ImportService) {
+              private ImportService : ImportService,
+              private DynamicTypesService : DynamicTypesService) {
     super(NavigationService);
   }
 
@@ -32,11 +38,22 @@ export class CreateWizardComponent extends NavigatableComponent
     this.repoStatusSubscription = this.itemRepository.getRepoStatusSubject()
       .subscribe((update) => {
       if (update.connected) {
-        this.types = this.itemRepository.getProxyFor('Model-Definitions').
+        this.models = this.itemRepository.getProxyFor('Model-Definitions').
           getDescendants().sort((first: ItemProxy, second: ItemProxy) => {
           return ((first.item.name > second.item.name) ?
             1 : ((first.item.name < second.item.name) ? -1 : 0));
         });
+        for (let i = 0; i < this.models.length; i++) {
+          let modelView = this.DynamicTypesService.getViewProxyFor(this.types[i]);
+          if (modelView) {
+            this.types.push(modelView);
+          }
+        }
+        this.recentProxies = this.itemRepository.getRecentProxies();
+        this.selectedType = this.types[0];
+        this.selectedParent = this.recentProxies[0];
+        console.log(this.selectedType);
+        console.log(this.selectedParent);
       }
     });
   }
