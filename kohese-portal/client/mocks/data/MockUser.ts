0424dc89dd3ec9a36dc26ac624747f5cc0f7c07d
Josh P <jephillips@johnny12.cfi.internal>
2018-02-20 12:42:08 -0600

    Fixed failing Details View spec setup

---
+++ kohese-portal/client/mocks/data/MockUser.ts
@@ -0,0 +1,13 @@
+export let MockUserData = {
+  "password": "$2a$10$6mt3XNXCjwYE8Mot9osp5eiHQjhbz9KY/4WYqrGm2OjapGmjosZRy",
+  "email": "test@test.net",
+  "id": "04b39d10-b9a2-11e7-b085-f7379f8cd936",
+  "name": "Test-User",
+  "description": "Test",
+  "parentId": "7480a310-b509-11e7-ba1e-cbd891de1f92",
+  "createdBy": "admin",
+  "createdOn": 1508949117209,
+  "modifiedBy": "jephillips",
+  "modifiedOn": 1508966827019,
+  "itemIds": []
+}
