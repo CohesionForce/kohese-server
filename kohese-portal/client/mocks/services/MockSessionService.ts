5dbc06cbfaa48b2b02ee9c9e348ed9b9484b688c
Josh P <jephillips@johnny12.cfi.internal>
2018-02-21 10:30:09 -0600

    Fixed details instantiation test

--- kohese-portal/client/mocks/services/MockSessionService.ts
+++ kohese-portal/client/mocks/services/MockSessionService.ts
@@ -1,8 +1,13 @@
 import { MockUserData } from '../data/MockUser';
 import * as ItemProxy from '../../../common/models/item-proxy';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 export class MockSessionService {
   getUsers() {
     return [new ItemProxy('KoheseUser', MockUserData)]
+  }
+
+  getSessionUser() {
+    return new BehaviorSubject<ItemProxy>(new ItemProxy('KoheseUser', MockUserData));
   }
 }
