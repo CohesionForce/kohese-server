0e8948dceac703bc592a677df4840c2617b3a7a9
Josh P <jephillips@johnny12.cfi.internal>
2018-02-21 14:31:22 -0600

    Add init tests for ChildrenTable, ProxySelector, and HistoryTab

---
+++ kohese-portal/client/src/components/details/children-tab/children-table/children-table.component.spec.ts
@@ -0,0 +1,49 @@
+import { TestBed, ComponentFixture} from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { MaterialModule } from '../../../../material.module'
+import { PipesModule } from '../../../../pipes/pipes.module';
+
+import { SessionService } from '../../../../services/user/session.service';
+
+import { ChildrenTableComponent } from './children-table.component';
+import { MockItemRepository } from '../../../../../mocks/services/MockItemRepository';
+
+import { NavigationService } from '../../../../services/navigation/navigation.service';
+import { MockNavigationService } from '../../../../../mocks/services/MockNavigationService';
+import { BehaviorSubject } from 'rxjs';
+
+describe('Component: Children Table', ()=>{
+  let childrenTableComponent: ChildrenTableComponent;
+  let childrenTableFixture : ComponentFixture<ChildrenTableComponent>;
+
+  beforeEach(()=>{
+    TestBed.configureTestingModule({
+      declarations: [ChildrenTableComponent],
+      imports : [CommonModule,
+         MaterialModule,
+         BrowserAnimationsModule,
+         PipesModule
+         ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        {provide: NavigationService, useClass: MockNavigationService}
+      ]
+    }).compileComponents();
+
+    childrenTableFixture = TestBed.createComponent(ChildrenTableComponent);
+    childrenTableComponent = childrenTableFixture.componentInstance;
+
+    childrenTableComponent.itemProxy = new MockItemRepository().getRootProxy();
+    childrenTableComponent.filterSubject = new BehaviorSubject<string>('');
+
+    childrenTableFixture.detectChanges();
+    
+  })
+
+  it('instantiates the ChildrenTable component', ()=>{
+    expect(childrenTableComponent).toBeTruthy(); 
+  })
+})
