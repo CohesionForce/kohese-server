004a1521a65c76d881753ca24548589ad20f994d
Josh P <jephillips@johnny12.cfi.internal>
2018-02-28 16:40:27 -0600

    Overwrode change detection for the children tab in details form

--- kohese-portal/client/src/components/details/children-tab/children-tab.component.ts
+++ kohese-portal/client/src/components/details/children-tab/children-tab.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit, OnDestroy, Input, EventEmitter, SimpleChange, OnChanges } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, EventEmitter, SimpleChange, OnChanges, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 
 import { NavigatableComponent } from '../../../classes/NavigationComponent.class'
 import { NavigationService } from '../../../services/navigation/navigation.service';
 
-import { ItemProxy } from '../../../../../common/models/item-proxy.js';
+import * as ItemProxy from '../../../../../common/models/item-proxy.js';
 import { DialogService } from '../../../services/dialog/dialog.service';
 import { ItemRepository } from '../../../services/item-repository/item-repository.service';
 import { Subscription } from 'rxjs/Subscription';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { CreateWizardComponent } from '../../create-wizard/create-wizard.component';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector : 'children-tab',
-  templateUrl : './children-tab.component.html'
+  templateUrl : './children-tab.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChildrenTabComponent extends NavigatableComponent
-                                  implements OnInit, OnDestroy, OnChanges{
+                                  implements OnInit, OnDestroy {
 
   /* Ui Switches */
   orderedChildren : boolean;
@@ -24,10 +26,9 @@ export class ChildrenTabComponent extends NavigatableComponent
 
   /* Data */
   @Input()
+  proxyStream : Observable<ItemProxy>;
+
   itemProxy : ItemProxy;
-  @Input() 
-  children : ItemProxy;
-  
   filterString : string;
   initialized : boolean = false;
   itemSortField : string;
@@ -35,6 +36,7 @@ export class ChildrenTabComponent extends NavigatableComponent
   /* Observables */
   saveEmitter : EventEmitter<ItemProxy>;
   filterSubject : BehaviorSubject<string>;
+  childrenStream : BehaviorSubject<ItemProxy> = new BehaviorSubject([]);
 
 
   /* Subscriptions */
@@ -43,11 +45,15 @@ export class ChildrenTabComponent extends NavigatableComponent
 
   constructor(protected NavigationService : NavigationService,
               private DialogService : DialogService,
-              private ItemRepository : ItemRepository) {
+              private ItemRepository : ItemRepository,
+              private changeRef : ChangeDetectorRef) {
     super(NavigationService);
   }
 
   ngOnInit() {
+    this.proxyStream.subscribe((newProxy : ItemProxy) => {
+     this.updateProxy(newProxy); 
+    })
     this.orderedChildren = this.itemProxy.childrenAreManuallyOrdered();
     this.repoReadySub = this.ItemRepository.getRepoStatusSubject()
       .subscribe(update => {
@@ -64,11 +70,10 @@ export class ChildrenTabComponent extends NavigatableComponent
     this.repoReadySub.unsubscribe();
   }
 
-  ngOnChanges (changes) {
-    console.log(changes)
-    if(this.initialized) {
-      this.itemProxy = (changes.itemProxy) ? changes.itemProxy.currentValue : changes.currentValue;
-    }
+  updateProxy (newProxy : ItemProxy) {
+    this.itemProxy = newProxy;
+    this.childrenStream.next(this.itemProxy.children);
+    this.changeRef.markForCheck();
   }
 
   onFilterUpdate (filter : string) {
