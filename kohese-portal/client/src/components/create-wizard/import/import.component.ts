f8ca26c4755ef0194c85bb92e7fe905a6ee80cc3
Josh P <jephillips@johnny12.cfi.internal>
2018-03-01 13:08:35 -0600

    Removed template binding to external services in import component

--- kohese-portal/client/src/components/create-wizard/import/import.component.ts
+++ kohese-portal/client/src/components/create-wizard/import/import.component.ts
@@ -3,16 +3,20 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 import { ItemRepository } from '../../../services/item-repository/item-repository.service';
 import { DynamicTypesService } from '../../../services/dynamic-types/dynamic-types.service';
 import { ImportService } from '../../../services/import/import.service';
+import { KoheseType } from '../../../classes/UDT/KoheseType.class';
 
 @Component({
   selector: 'import',
   templateUrl: './import.component.html'
 })
 export class ImportComponent {
+  itemType : KoheseType;
+
   constructor(@Optional() @Inject(MAT_DIALOG_DATA) private data: any,
     private itemRepository: ItemRepository,
     private typeService: DynamicTypesService,
-    private ImportService : ImportService) {
+    private importService : ImportService) {
+      this.itemType = typeService.getKoheseTypes['Item'];
   }
 
   filter(fieldName: string): boolean {
@@ -26,5 +30,10 @@ export class ImportComponent {
     }
 
     return names.join('\n');
+  }
+
+  importFile (fileInput, userInput) {
+    this.importService.importFile(fileInput.files, 
+                                  userInput.getFormGroup().value.parentId)
   }
 }
