debbd0524967a110f311fe6a2fe072d6f55bf24e
David R. Phillips <dphillips@CohesionForce.com>
2018-04-03 16:27:43 -0500

    Remove ng1 client

--- kohese-portal/client-ng1/components/common/constants/endpoints.js
+++
@@ -1,6 +0,0 @@
-/* Collection of endpoint constants */
-
-export default ()=>{
-  angular.module('app.constants.endpoints',[])
-    .constant('API_URL', '');
-}
--- kohese-portal/client-ng1/components/common/directives/quick-item/quick-item.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by josh on 9/30/15.
- */
