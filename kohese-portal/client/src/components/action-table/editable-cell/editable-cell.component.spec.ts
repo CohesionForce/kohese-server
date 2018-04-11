7979345f05ff93a430fbd1d6f76a5f1c6437d592
Josh P <jephillips@johnny12.cfi.internal>
2018-04-11 15:31:16 -0500

    More editable cell testing

--- kohese-portal/client/src/components/action-table/editable-cell/editable-cell.component.spec.ts
+++ kohese-portal/client/src/components/action-table/editable-cell/editable-cell.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed, ComponentFixture} from '@angular/core/testing';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { TestBed, ComponentFixture, async} from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, SimpleChanges, SimpleChange } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
@@ -8,14 +8,16 @@ import { EditableCellComponent } from './editable-cell.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import * as ItemProxy from '../../../../../common/src/item-proxy';
+import * as KoheseModel from '../../../../../common/src/KoheseModel';
 import { MockItem } from '../../../../mocks/data/MockItem';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { MockDataModel } from '../../../../mocks/data/MockDataModel';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 describe('Component: Editable Cell ', ()=>{
   let editableCellComponent: EditableCellComponent;
   let editableCellFixture : ComponentFixture<EditableCellComponent>;
 
-  beforeEach(()=>{
+  beforeEach(async(()=>{
     TestBed.configureTestingModule({
       declarations: [EditableCellComponent],
       imports : [CommonModule,
@@ -36,19 +38,30 @@ describe('Component: Editable Cell ', ()=>{
       depth : 0,
       proxy : new ItemProxy('Item', MockItem())
     };
-    editableCellComponent.action.proxy.model = {
-        internal : true     
-    }
+    editableCellComponent.action.proxy.model = new KoheseModel(MockDataModel());
     editableCellComponent.editableStream = new BehaviorSubject<boolean>(true);
-    editableCellComponent.rowActionStream = new Observable<any>();    
+    editableCellComponent.rowActionStream = new Subject<any>();    
 
     editableCellFixture.detectChanges();
     
-  })
+  }))
 
   it('instantiates the editableCell component', ()=>{
     expect(editableCellComponent).toBeTruthy(); 
   })
+
+  it('updates when a new action is provided', async(()=>{
+    let newProxy = new ItemProxy('Item', MockItem());
+    newProxy.item.name = 'New Action';
+    newProxy.item.model = new ItemProxy('KoheseModel', MockDataModel());
+    editableCellComponent.action = {
+      depth : 0,
+      proxy : newProxy
+    }
+    editableCellComponent.ngOnChanges({
+      name: new SimpleChange(null, newProxy, true)
+    })
+  }))
 
   afterEach(()=>{
     ItemProxy.getWorkingTree().reset();
