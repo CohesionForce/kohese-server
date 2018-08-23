19165732f637fe00623cfaf14d2156ab605f27bb
Joshua E. Phillips <jephillips@CohesionForce.com>
2018-08-23 11:15:21 -0500

    Add column format definition

--- kohese-portal/client/src/components/type-editor/format-editor/format-gui/property-container/column-container-editor/column-container-editor.component.ts
+++ kohese-portal/client/src/components/type-editor/format-editor/format-gui/property-container/column-container-editor/column-container-editor.component.ts
@@ -1,6 +1,17 @@
+import { PropertyDefinition } from './../../../format-editor.component';
 import { KoheseType } from '../../../../../../classes/UDT/KoheseType.class';
 import { FormatContainer } from '../../../format-editor.component';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+export interface ColumnContainer extends FormatContainer {
+  numColumns : Number
+  columns : Array<ColumnDefinition>
+}
+
+interface ColumnDefinition {
+  contents : Array<PropertyDefinition>;
+  index : number
+}
 
 @Component({
   selector: 'column-container-editor',
@@ -9,13 +20,56 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ColumnContainerEditorComponent implements OnInit {
   @Input()
-  container : FormatContainer;
+  container : ColumnContainer;
   @Input()
   kind : KoheseType
+  @Output()
+  deleted = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
+    if (!this.container.numColumns) {
+      this.container.numColumns = 2;
+    }
+    if (!this.container.columns) {
+      this.initColumns(this.container);
+    }
+  }
+
+
+
+  initColumns(container) {
+    for (let idx = 0; idx < container.numColumns; idx++) {
+      if (!this.container.columns) {
+        this.container.columns = [];
+      }
+      if (!this.container.columns[idx]) {
+        this.container.columns[idx] = {
+          index : idx,
+          contents : []
+        }
+      }
+    }
+  }
+
+  addRow(rowNum : number) {
+    this.container.columns[rowNum].contents.push({
+      propertyName : '',
+      hideLabel : false,
+      labelOrientation: 'Top',
+      kind :  ''
+    })
+  }
+
+
+  removeContainer(row) {
+    this.deleted.emit(this.container);
+  }
+
+  setFormat(numColumns : number) {
+    this.container.numColumns = numColumns
+    this.initColumns(this.container);
   }
 
 }
