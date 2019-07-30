82339c26926c2e4e657d464887a732db0054eceb
dstockman <dstockman@yondu.internal.cfi>
2019-07-30 13:53:10 -0500

    Added initial implementation of Item delineation on the Document page

--- kohese-portal/client/src/components/document/document.component.ts
+++ kohese-portal/client/src/components/document/document.component.ts
@@ -8,7 +8,8 @@ import { ItemRepository } from '../../services/item-repository/item-repository.s
 import { NavigationService } from '../../services/navigation/navigation.service';
 import { DocumentConfigurationEditorComponent } from '../object-editor/document-configuration/document-configuration-editor.component';
 import { TreeComponent } from '../tree/tree.component';
-import { TextEditorComponent } from '../text-editor/text-editor.component';
+import { TextEditorComponent,
+  FormatSpecification } from '../text-editor/text-editor.component';
 import { ItemProxy } from '../../../../common/src/item-proxy';
 import { TreeConfiguration } from '../../../../common/src/tree-configuration';
 import { LocationMap } from '../../constants/LocationMap.data';
@@ -108,8 +109,13 @@ export class DocumentComponent implements OnInit, OnDestroy {
     return Object;
   }
   
+  private static readonly _OPENING_HIDDEN_TAG: string =
+    '<div id="" style="visibility: hidden;"><div id="delineator" class="mceNonEditable" style="display: none; color: lightgray; text-align: center;">------------------------</div>\n\n';
+  private static readonly _CLOSING_HIDDEN_TAG: string = '</div>\n\n';
   private static readonly _SEPARATOR_DIV_REGEXP: RegExp =
-    /<div id="([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})" style="visibility: hidden;">\s{2}/g;
+    /<div id="([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})" style="visibility: hidden;"><div id="[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}delineator" class="mceNonEditable" style="display: none; color: lightgray; text-align: center;">-{12}\s\S+-{12}<\/div>\s{2}/g;
+  private static readonly _ATTRIBUTE_REGEXP: RegExp =
+    /<div id="[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}[\s\S]+" style="visibility: hidden;"><div id="[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}delineator" class="mceNonEditable" style="display: none; color: lightgray; text-align: center;">-{12}\s\S+-{12}<\/div>\s{2}/g;
   
   public constructor(private _changeDetectorRef: ChangeDetectorRef,
     @Optional() @Inject(MAT_DIALOG_DATA) private _data: any,
@@ -188,17 +194,18 @@ export class DocumentComponent implements OnInit, OnDestroy {
       let itemProxy: ItemProxy = TreeConfiguration.getWorkingTree().
         getProxyFor(documentIds[j]);
       
-      document += ('<div id="' + itemProxy.item.id +
-        '" style="visibility: hidden;">\n\n');
+      document += this.getFormattedOpeningHiddenTag(itemProxy.item.id,
+        itemProxy.item.name);
+      document += this.getFormattedOpeningHiddenTag(itemProxy.item.id +
+        'description', 'Description');
       
       if (itemProxy.item.description) {
-        document += itemProxy.item.description;
-      }
-      if (j < (documentIds.length - 1)) {
-        document += '\n\n';
+        document += (itemProxy.item.description + '\n\n');
       }
       
-      document += '</div>\n\n';
+      document += DocumentComponent._CLOSING_HIDDEN_TAG;
+      
+      document += DocumentComponent._CLOSING_HIDDEN_TAG;
     }
     
     return document;
@@ -269,12 +276,8 @@ export class DocumentComponent implements OnInit, OnDestroy {
   
   public selectItemInOutline(node: any): void {
     let id: string;
-    // Reuse _SEPARATOR_DIV_REGEXP, trimming off '>\s{2}'.
-    let separatorDivRegExpSource: string = DocumentComponent.
-      _SEPARATOR_DIV_REGEXP.source;
-    let startsWithSeparatorRegExp: RegExp = new RegExp('^' +
-      separatorDivRegExpSource.substring(0, separatorDivRegExpSource.length -
-      6));
+    let startsWithSeparatorRegExp: RegExp =
+      /^<div id="([0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12})" style="visibility: hidden;"/;
     let match: any;
     do {
       if ((match = startsWithSeparatorRegExp.exec(node.outerHTML)) !== null) {
@@ -292,30 +295,74 @@ export class DocumentComponent implements OnInit, OnDestroy {
     }
   }
   
-  public getUnifiedDocumentFunction(): (text: string) => string {
+  public getFormattedTextFunction(): (text: string, formatSpecification:
+    FormatSpecification) => string {
     /* The below function is passed to text-editor, so bind the correct 'this'
     to that function. */
-    return ((text: string) => {
-      // Remove the last '</div>\n\n'.
-      let regExpTarget: string = text.substring(0, text.length - 8);
-      let ids: Array<string> = [];
-      // Reverse regExpTarget to remove inserted '</div>\n\n's in reverse.
-      let document: string = regExpTarget.split('').reverse().join('');
-      let match: any;
-      while ((match = DocumentComponent._SEPARATOR_DIV_REGEXP.exec(
-        regExpTarget)) != null) {
-        ids.push(match[1]);
-        if (match.index !== 0) {
-          document = document.substring(0, (regExpTarget.length - match.
-            index)) + document.substring(regExpTarget.length - match.index +
-            8);
+    return ((text: string, formatSpecification: FormatSpecification) => {
+      let formattedText: string = text;
+      if (formatSpecification.removeExternalFormatting) {
+        // Remove the last '</div>\n\n'.
+        let regExpTarget: string = formattedText.substring(0, formattedText.
+          length - 8);
+        let ids: Array<string> = [];
+        // Reverse regExpTarget to remove inserted '</div>\n\n's in reverse.
+        let document: string = regExpTarget.split('').reverse().join('');
+        let match: any;
+        while ((match = DocumentComponent._SEPARATOR_DIV_REGEXP.exec(
+          regExpTarget)) != null) {
+          ids.push(match[1]);
+          if (match.index !== 0) {
+            document = document.substring(0, (regExpTarget.length - match.
+              index)) + document.substring(regExpTarget.length - match.index +
+              8);
+          }
         }
+        // Re-orient document.
+        document = document.split('').reverse().join('');
+        document = document.replace(DocumentComponent._SEPARATOR_DIV_REGEXP, '');
+        
+        formattedText = document;
       }
-      // Re-orient document.
-      document = document.split('').reverse().join('');
-      document = document.replace(DocumentComponent._SEPARATOR_DIV_REGEXP, '');
       
-      return document;
+      if (formatSpecification.updateSource) {
+        this._document = formattedText;
+        
+        if (formatSpecification.delineate !== undefined) {
+          this._changeDetectorRef.detectChanges();
+          
+          let componentIds: Array<string> = Object.keys(this.
+            _documentConfiguration.components);
+          for (let j: number = 0; j < componentIds.length; j++) {
+            let element: any = this._textEditor.editor.editor.dom.select(
+              'div#' + componentIds[j] + 'delineator')[0];
+            if (element) {
+              this._textEditor.editor.editor.dom.setStyle(element, 'display',
+                (formatSpecification.delineate ? '' : 'none'));
+            }
+            
+            let componentSettings: any = this._documentConfiguration.
+              components[componentIds[j]];
+            if (componentSettings.includeDescendants) {
+              let itemProxy: ItemProxy = TreeConfiguration.getWorkingTree().
+                getProxyFor(componentIds[j]);
+              itemProxy.visitTree({ includeOrigin: false },
+                (descendantItemProxy: ItemProxy) => {
+                element = this._textEditor.editor.editor.dom.select('div#' +
+                  descendantItemProxy.item.id + 'delineator')[0];
+                if (element) {
+                  this._textEditor.editor.editor.dom.setStyle(element,
+                    'display', (formatSpecification.delineate ? '' : 'none'));
+                }
+              });
+            }
+          }
+        }
+        
+        this._changeDetectorRef.markForCheck();
+      }
+      
+      return formattedText;
     }).bind(this);
   }
   
@@ -338,14 +385,25 @@ export class DocumentComponent implements OnInit, OnDestroy {
       }
       // Re-orient splitTarget.
       splitTarget = splitTarget.split('').reverse().join('');
-      let descriptions: Array<string> = splitTarget.split(DocumentComponent.
-        _SEPARATOR_DIV_REGEXP);
+      
+      let itemIdsAndContent: Array<string> = splitTarget.split(
+        DocumentComponent._SEPARATOR_DIV_REGEXP);
       // Remove the first element, as it should be empty.
-      descriptions.shift();
+      itemIdsAndContent.shift();
       let documentMap: Map<string, string> = new Map<string, string>();
-      for (let j: number = 0; j < descriptions.length; j++) {
+      for (let j: number = 0; j < itemIdsAndContent.length; j++) {
         if ((j % 2) === 0) {
-          documentMap.set(descriptions[j], descriptions[j + 1]);
+          let itemId: string = itemIdsAndContent[j];
+          let description: string = itemIdsAndContent[j + 1];
+          let descriptionHiddenTag: string = this.getFormattedOpeningHiddenTag(
+            itemId + 'description', 'Description');
+          // Remove non-description content
+          description = description.substring(description.indexOf(
+            descriptionHiddenTag) + descriptionHiddenTag.length);
+          description = description.substring(0, description.search(
+            DocumentComponent._ATTRIBUTE_REGEXP) - 8);
+          
+          documentMap.set(itemId, description);
         }
       }
       
@@ -381,5 +439,21 @@ export class DocumentComponent implements OnInit, OnDestroy {
         }
       }
     }).bind(this);
+  }
+  
+  private getFormattedOpeningHiddenTag(id: string, text: string): string {
+    // Insert id after 'id="'.
+    let formattedOpeningHiddenTag: string = (DocumentComponent.
+      _OPENING_HIDDEN_TAG.substring(0, 9) + id + DocumentComponent.
+      _OPENING_HIDDEN_TAG.substring(9, 48) + id + DocumentComponent.
+      _OPENING_HIDDEN_TAG.substring(48));
+    
+    // Insert text in the middle of the 24 '-'s.
+    formattedOpeningHiddenTag = formattedOpeningHiddenTag.substring(0,
+      formattedOpeningHiddenTag.length - 20) + text +
+      formattedOpeningHiddenTag.substring(formattedOpeningHiddenTag.length -
+      20);
+    
+    return formattedOpeningHiddenTag;
   }
 }
