83ee9c25a54f4957c34bfa7a2aafc03c283b0bdb
dstockman <dstockman@yondu.internal.cfi>
2019-11-19 15:02:58 -0600

    Added support for multiple List containers in a Format Definition
    This commit also includes testing-related changes.

--- kohese-portal/client/src/components/dashboard/project-dashboard/status-dashboard/state-bar-chart/state-summary-dialog/state-summary-dialog.component.spec.ts
+++ kohese-portal/client/src/components/dashboard/project-dashboard/status-dashboard/state-bar-chart/state-summary-dialog/state-summary-dialog.component.spec.ts
@@ -3,6 +3,12 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { MAT_DIALOG_DATA } from '@angular/material';
 
+import { ItemRepository } from '../../../../../../services/item-repository/item-repository.service';
+import { MockItemRepository } from '../../../../../../../mocks/services/MockItemRepository';
+import { NavigationService } from '../../../../../../services/navigation/navigation.service';
+import { MockNavigationService } from '../../../../../../../mocks/services/MockNavigationService';
+import { DialogService } from '../../../../../../services/dialog/dialog.service';
+import { MockDialogService } from '../../../../../../../mocks/services/MockDialogService';
 import { StateSummaryDialogComponent } from './state-summary-dialog.component';
 
 describe('StateSummaryDialogComponent', () => {
@@ -14,13 +20,17 @@ describe('StateSummaryDialogComponent', () => {
       declarations: [ StateSummaryDialogComponent ],
       imports: [ NgxDatatableModule ],
       providers: [ {
-        provide: MAT_DIALOG_DATA,
-        useValue: {
-          proxies: [],
-          kindName: 'Item',
-          stateName: 'Approved'
-        }
-      } ],
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            proxies: [],
+            kindName: 'Item',
+            stateName: 'Approved'
+          }
+        },
+        { provide: ItemRepository, useClass: MockItemRepository },
+        { provide: NavigationService, useClass: MockNavigationService },
+        { provide: DialogService, useClass: MockDialogService }
+      ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
