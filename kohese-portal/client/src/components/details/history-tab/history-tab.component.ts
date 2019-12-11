c7d23a0d6cbaa08c2aafb780146c8d87dc79080f
dstockman <dstockman@yondu.internal.cfi>
2019-12-11 09:54:38 -0600

    Add buttons to expand and collapse all versions on the History tab
    This commit also includes re-adding certain Item information to the
    History tab.

--- kohese-portal/client/src/components/details/history-tab/history-tab.component.ts
+++ kohese-portal/client/src/components/details/history-tab/history-tab.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, ChangeDetectionStrategy,
+  ChangeDetectorRef, ViewChildren, QueryList } from '@angular/core';
+import { MatExpansionPanel } from '@angular/material';
 import { BehaviorSubject, Subscription } from 'rxjs';
 
 import { NavigatableComponent } from '../../../classes/NavigationComponent.class'
@@ -13,6 +15,7 @@ import { KoheseCommit } from '../../../../../common/src/kohese-commit';
 @Component({
   selector: 'history-tab',
   templateUrl: './history-tab.component.html',
+  styleUrls: ['./history-tab.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HistoryTabComponent extends NavigatableComponent
@@ -24,6 +27,9 @@ export class HistoryTabComponent extends NavigatableComponent
   get versions() {
     return this._versions;
   }
+  
+  @ViewChildren(MatExpansionPanel)
+  private _expansionPanels: QueryList<MatExpansionPanel>;
   
   private _differenceMap: Map<string, any> = new Map<string, any>();
   get differenceMap() {
@@ -53,27 +59,37 @@ export class HistoryTabComponent extends NavigatableComponent
     this.streamSub.unsubscribe();
   }
   
+  public changeAllExpansionStates(expand: boolean): void {
+    let expansionPanels: Array<MatExpansionPanel> = this.
+      _expansionPanels.toArray();
+    for (let j: number = 0; j < expansionPanels.length; j++) {
+      expansionPanels[j].expanded = expand;
+    }
+    
+    this.changeRef.markForCheck();
+  }
+  
   public async compare(version: any): Promise<void> {
     let itemProxy: ItemProxy = this.proxyStream.getValue();
-    let baseKoheseCommit: KoheseCommit = await ItemCache.getItemCache().
+    let changeKoheseCommit: KoheseCommit = await ItemCache.getItemCache().
       getCommit(version.commit);
-    let baseBlob: any = (await baseKoheseCommit.getTreeHashMap())[itemProxy.
+    let changeBlob: any = (await changeKoheseCommit.getTreeHashMap())[itemProxy.
       item.id];
-    let changeBlob: any;
-    for (let j: number = 0; j < baseKoheseCommit.parents.length; j++) {
-      let changeKoheseCommit: KoheseCommit = await ItemCache.getItemCache().
-        getCommit(baseKoheseCommit.parents[j]);
-      changeBlob = (await changeKoheseCommit.getTreeHashMap())[itemProxy.item.
+    let baseBlob: any;
+    for (let j: number = 0; j < changeKoheseCommit.parents.length; j++) {
+      let baseKoheseCommit: KoheseCommit = await ItemCache.getItemCache().
+        getCommit(changeKoheseCommit.parents[j]);
+      baseBlob = (await baseKoheseCommit.getTreeHashMap())[itemProxy.item.
         id];
-      if (changeBlob) {
+      if (baseBlob) {
         break;
       }
     }
     
     this._differenceMap.set(version.commit, await Compare.compareItems(
-      itemProxy.item.id, baseBlob.kind, baseBlob.oid, itemProxy.item.id,
-      (changeBlob ? changeBlob.kind : undefined), (changeBlob ? changeBlob.
-      oid : undefined), this._dynamicTypesService));
+      itemProxy.item.id, (baseBlob ? baseBlob.kind : undefined), (baseBlob ?
+      baseBlob.oid : undefined), itemProxy.item.id, changeBlob.kind,
+      changeBlob.oid, this._dynamicTypesService));
     this.changeRef.markForCheck();
   }
 }
