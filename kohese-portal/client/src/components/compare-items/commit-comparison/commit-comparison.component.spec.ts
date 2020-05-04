05dbed25ddfb898ce25ff8f69cfa1136c02bf0cf
David R. Phillips <dphillips@CohesionForce.com>
2020-05-04 12:49:16 -0500

    Fix commit related UI tests Task: Resolve Commit Comparison UI Test Case Failures (a82844a0-00ea-11ea-8207-9904d90014bc)

--- kohese-portal/client/src/components/compare-items/commit-comparison/commit-comparison.component.spec.ts
+++ kohese-portal/client/src/components/compare-items/commit-comparison/commit-comparison.component.spec.ts
@@ -19,8 +19,8 @@ describe('Component: commit-comparison', () => {
       providers: [ {
           provide: MAT_DIALOG_DATA,
           useValue: {
-            baseCommitId: 'cca8746527cb48d3e8a815c12b99bc1cb378b9f0',
-            changeCommitId: 'ccdaa0ec5f01db40886fe52e917c336eb7c075ea'
+            baseCommitId: '7ef7525795a5c370b0abfa501ab87324f5ce5908',
+            changeCommitId: '42a8e801f9efef73db114730d5819997e38916d7'
           }
         }, { provide: DynamicTypesService, useClass: MockDynamicTypesService }
       ],
@@ -39,11 +39,20 @@ describe('Component: commit-comparison', () => {
     fixture.detectChanges();
   });
   
-  it('compares two commits', () => {
-    let comparisons: Array<Comparison> = component.comparisonsSubject.
-      getValue();
-    expect(comparisons.length).toBeGreaterThan(0);
-    let comparison: Comparison = comparisons[0];
-    expect(comparison.changeTypes.length).toBeGreaterThan(0);
+  it('compares two commits', (done) => {
+    let firstTime : boolean = true;
+    let subscription = component.comparisonsSubject.subscribe((comparisons: Array<Comparison>) => {
+      if (firstTime && comparisons.length === 0){
+        // Ignore an empty list if this the firstTime since the comparision is async and may not have finished
+      } else {
+        expect(comparisons.length).toBeGreaterThan(0);
+        console.log(comparisons);
+        let comparison: Comparison = comparisons[0];
+        expect(comparison.changeTypes.length).toBeGreaterThan(0);
+        subscription.unsubscribe();
+        done();
+      }
+      firstTime = false;
+    });
   });
 });
