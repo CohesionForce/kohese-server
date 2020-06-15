ec2312fa64b13b23024b6a15653264536cf29338
Thomas Pitts <tpitts@cohesionforce.com>
2020-06-15 17:51:52 -0500

    Test Cases for Journal Component Task: Implement test cases for Journal component (2a7ea430-99e3-11ea-93f7-0b2ad0b1b912) * Added tests for sorting * Added export for JournalOrdering enum

--- kohese-portal/client/src/components/details/journal/journal.component.spec.ts
+++ kohese-portal/client/src/components/details/journal/journal.component.spec.ts
@@ -1,8 +1,13 @@
-import { TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import {
+  TestBed,
+  ComponentFixture,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '../../../material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PipesModule } from '../../../pipes/pipes.module';
@@ -13,7 +18,7 @@ import { MockDataModel } from '../../../../mocks/data/MockDataModel';
 import { ItemProxy } from '../../../../../common/src/item-proxy';
 import { TreeConfiguration } from '../../../../../common/src/tree-configuration';
 
-import { JournalComponent } from './journal.component';
+import { JournalComponent, JournalOrdering } from './journal.component';
 import { DialogService } from '../../../services/dialog/dialog.service';
 import { MockDialogService } from '../../../../mocks/services/MockDialogService';
 import { ItemRepository } from '../../../services/item-repository/item-repository.service';
@@ -23,66 +28,157 @@ import { SessionService } from '../../../services/user/session.service';
 import { MockSessionService } from '../../../../mocks/services/MockSessionService';
 
 // TODO: This needs to be implemented
-describe('Component: Journal', ()=>{
+fdescribe('Component: Journal', () => {
   let journalComponent: JournalComponent;
-  let journalFixture : ComponentFixture<JournalComponent>;
+  let journalFixture: ComponentFixture<JournalComponent>;
 
-  beforeEach(()=>{
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [JournalComponent],
-      imports : [CommonModule,
-         MaterialModule,
-         BrowserAnimationsModule,
-         FormsModule,
-         ReactiveFormsModule,
-         PipesModule
-         ],
+      imports: [
+        CommonModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        PipesModule,
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       providers: [
-        {provide : DialogService, useClass: MockDialogService},
-        {provide : ItemRepository, useClass: MockItemRepository},
+        { provide: DialogService, useClass: MockDialogService },
+        { provide: ItemRepository, useClass: MockItemRepository },
         { provide: NavigationService, useClass: MockNavigationService },
-        { provide: SessionService, useClass: MockSessionService }
-      ]
+        { provide: SessionService, useClass: MockSessionService },
+      ],
     }).compileComponents();
 
     journalFixture = TestBed.createComponent(JournalComponent);
     journalComponent = journalFixture.componentInstance;
 
-    journalComponent.itemProxy = MockItemRepository.singleton.getProxyFor('test-uuid6');
+    journalComponent.itemProxy = MockItemRepository.singleton.getProxyFor(
+      'test-uuid6'
+    );
+
+    try {
+      // Date must be past 500,000 to return valid 600456789000
+
+      //Tuesday, January 10, 1989 5:33:09 PM
+      let obs1 = new ItemProxy('Observation', {
+        name: 'Observation1',
+        id: 'obs1',
+        observedBy: 'admin',
+        observedOn: 600456789000,
+        createdBy: 'dphillips',
+        createdOn: 600456789000,
+        context: [{ id: 'test-uuid6' }],
+      });
+      obs1.item.modifiedOn = 600486789000;
+
+      //Tuesday, January 10, 1989 5:49:49 PM
+      let obs2 = new ItemProxy('Observation', {
+        name: 'Observation2',
+        id: 'obs2',
+        observedBy: 'guser',
+        observedOn: 600457789000,
+        createdBy: 'ephillips',
+        createdOn: 600457789000,
+        context: [{ id: 'test-uuid6' }],
+      });
+      obs2.item.modifiedOn = 600476789000;
+
+      //Tuesday, January 10, 1989 5:34:59 PM
+      let iss1 = new ItemProxy('Issue', {
+        name: 'Issue1',
+        id: 'iss1',
+        observedBy: 'tuser',
+        observedOn: 600456899000,
+        createdBy: 'gphilliphs',
+        createdOn: 600456899000,
+        issueState: 'Observed',
+        context: [{ id: 'test-uuid6' }],
+      });
+
+    } catch (err) {
+      console.log('*** Error: ' + JSON.stringify(err));
+      console.log(err.stack);
+    }
 
     journalFixture.detectChanges();
+  });
 
-  })
-
-  it('instantiates the Journal component', ()=>{
+  it('instantiates the Journal component', () => {
     expect(JournalComponent).toBeTruthy();
   });
 
-  describe('sorting', ()=>{
-    it('sorts by eldest first when observed',()=>{
-      pending();
-    })
-    it('sorts by eldest last when observed', ()=>{
-      pending();
-    })
-    it('sorts by first journal entry made',()=>{
-      pending();
-    })
-    it('sorts by last journal entry made',()=>{
-      pending();
-    })
-    it('sorts by observer',()=>{
-      pending();
-    })
-    it('sorts by journal entry maker', ()=>{
-      pending();
-    })
-    it('sorts by issues first',()=>{
-      pending();
-    })
-    it('sorts by issues last', ()=>{
-      pending();
-    })
-  });  
-})
+  // Want to test by order of sorting
+  describe('sorting', () => {
+    function retrieveOrderedObservationIds(sortOrder: JournalOrdering) {
+
+      // Configure JournalComponent observation order
+      journalComponent.selectedOrdering = sortOrder;
+
+      // Retrieve mock observations with no Filter in getObervations parameter
+      const noFilter = '';
+      let observations = journalComponent.getObservations(noFilter);
+
+      // Iterates array observations and retrieve the itemId for each entry in order
+      let actualOrder = [];
+      observations.forEach((proxy) => {
+        actualOrder.push(proxy.item.id);
+      });
+
+      return actualOrder;
+    }
+
+    it('sorts by eldest first when observed', () => {
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.ELDEST_FIRST_WHEN_OBSERVED);
+      let expectedOrder = ['obs1', 'iss1', 'obs2'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+
+    it('sorts by eldest last when observed', () => {
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.ELDEST_LAST_WHEN_OBSERVED);
+      let expectedOrder = ['obs2', 'iss1', 'obs1'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+
+    it('sorts by first journal entry made', () => {
+      //createdOn attribute
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.ELDEST_FIRST_JOURNAL_ENTRY_MADE);
+      let expectedOrder = ['obs1', 'iss1', 'obs2'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+
+    it('sorts by last journal entry made', () => {
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.ELDEST_LAST_JOURNAL_ENTRY_MADE);
+      let expectedOrder = ['obs2', 'iss1', 'obs1'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+
+    it('sorts by observer', () => {
+      //ObservedBy
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.OBSERVER);
+      let expectedOrder = ['obs1', 'obs2', 'iss1'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+
+    it('sorts by journal entry maker', () => {
+      //Refers to the "createdBy" attribute
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.JOURNAL_ENTRY_MAKER);
+      let expectedOrder = ['obs1', 'obs2', 'iss1'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+
+    it('sorts by issues first', () => {
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.ISSUES_FIRST);
+      let expectedOrder = ['iss1', 'obs1', 'obs2'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+
+    it('sorts by issues last', () => {
+      let actualOrder = retrieveOrderedObservationIds(JournalOrdering.ISSUES_LAST);
+      let expectedOrder = ['obs1', 'obs2', 'iss1'];
+      expect(actualOrder).toEqual(expectedOrder);
+    });
+  });
+});
--- kohese-portal/client/src/components/details/journal/journal.component.ts
+++ kohese-portal/client/src/components/details/journal/journal.component.ts
@@ -11,12 +11,14 @@ import { FormatObjectEditorComponent } from '../../object-editor/format-object-e
 import { ItemProxy } from '../../../../../common/src/item-proxy';
 import { TreeConfiguration } from '../../../../../common/src/tree-configuration';
 
-enum Ordering {
+// TODO: Change Component to use selectedOrdering instead of exporting enumeration with Ordering getter
+export enum JournalOrdering {
   ELDEST_FIRST_WHEN_OBSERVED = 'Eldest First When Observed',
     ELDEST_LAST_WHEN_OBSERVED = 'Eldest Last When Observed',
     ELDEST_FIRST_JOURNAL_ENTRY_MADE = 'Eldest First Journal Entry Made',
     ELDEST_LAST_JOURNAL_ENTRY_MADE = 'Eldest Last Journal Entry Made',
-    OBSERVER = 'Observer', JOURNAL_ENTRY_MAKER = 'Journal Entry Maker',
+    OBSERVER = 'Observer',
+    JOURNAL_ENTRY_MAKER = 'Journal Entry Maker',
     ISSUES_FIRST = 'Issues First', ISSUES_LAST = 'Issues Last'
 }
 
@@ -35,7 +37,7 @@ export class JournalComponent {
   set itemProxy(itemProxy: ItemProxy) {
     this._itemProxy = itemProxy;
   }
-  
+
   private _selectedParentId: string = '';
   get selectedParentId() {
     return this._selectedParentId;
@@ -43,39 +45,39 @@ export class JournalComponent {
   set selectedParentId(selectedParentId: string) {
     this._selectedParentId = selectedParentId;
   }
-  
+
   private _additionalContextIds: Array<string> = [];
   get additionalContextIds() {
     return this._additionalContextIds;
   }
-  
-  private _selectedOrdering: Ordering = Ordering.ELDEST_FIRST_WHEN_OBSERVED;
+
+  private _selectedOrdering: JournalOrdering = JournalOrdering.ELDEST_FIRST_WHEN_OBSERVED;
   get selectedOrdering() {
     return this._selectedOrdering;
   }
-  set selectedOrdering(selectedOrdering: Ordering) {
+  set selectedOrdering(selectedOrdering: JournalOrdering) {
     this._selectedOrdering = selectedOrdering;
   }
-  
+
   private _filterTimeoutIdentifier: any;
-  
+
   private _editableSet: Array<string> = [];
   get editableSet() {
     return this._editableSet;
   }
-  
+
   get Ordering() {
-    return Ordering;
+    return JournalOrdering;
   }
-  
+
   get FormatDefinitionType() {
     return FormatDefinitionType;
   }
-  
+
   get TreeConfiguration() {
     return TreeConfiguration;
   }
-  
+
   get Object() {
     return Object;
   }
@@ -85,7 +87,7 @@ export class JournalComponent {
     DialogService, private _navigationService: NavigationService,
     private _sessionService: SessionService) {
   }
-  
+
   public addEntry(): void {
     let username: string = this._sessionService.user.name;
     let timestamp: number = Date.now();
@@ -114,21 +116,21 @@ export class JournalComponent {
       }
     });
   }
-  
+
   public filterChanged(filter: string): void {
     if (this._filterTimeoutIdentifier) {
       clearTimeout(this._filterTimeoutIdentifier);
     }
-    
+
     this._filterTimeoutIdentifier = setTimeout(() => {
       this._changeDetectorRef.reattach();
       this._changeDetectorRef.markForCheck();
       this._filterTimeoutIdentifier = undefined;
     }, 700);
-    
+
     this._changeDetectorRef.detach();
   }
-  
+
   public getObservations(filter: string): Array<ItemProxy> {
     let observationItemProxys: Array<ItemProxy> = [];
     TreeConfiguration.getWorkingTree().getRootProxy().visitTree(
@@ -140,7 +142,7 @@ export class JournalComponent {
           itemProxy.item.context = (itemProxy.item.context ? [itemProxy.item.
             context] : []);
         }
-        
+
         if (itemProxy.item.context.map((reference: any) => {
           // Migration code
           if (reference.id) {
@@ -153,46 +155,46 @@ export class JournalComponent {
         }
       }
     }, undefined);
-    
+
     return observationItemProxys.filter((observationItemProxy: ItemProxy) => {
       return (observationItemProxy.item.name.toLowerCase().indexOf(filter.
         toLowerCase()) !== -1);
     }).sort((oneObservationItemProxy: ItemProxy, anotherObservationItemProxy:
       ItemProxy) => {
       switch (this._selectedOrdering) {
-        case Ordering.ELDEST_FIRST_WHEN_OBSERVED:
+        case JournalOrdering.ELDEST_FIRST_WHEN_OBSERVED:
           return (oneObservationItemProxy.item.observedOn -
             anotherObservationItemProxy.item.observedOn);
-        case Ordering.ELDEST_LAST_WHEN_OBSERVED:
+        case JournalOrdering.ELDEST_LAST_WHEN_OBSERVED:
           return (anotherObservationItemProxy.item.observedOn -
             oneObservationItemProxy.item.observedOn);
-        case Ordering.ELDEST_FIRST_JOURNAL_ENTRY_MADE:
+        case JournalOrdering.ELDEST_FIRST_JOURNAL_ENTRY_MADE:
           return oneObservationItemProxy.item.createdOn -
             anotherObservationItemProxy.item.createdOn;
-        case Ordering.ELDEST_LAST_JOURNAL_ENTRY_MADE:
+        case JournalOrdering.ELDEST_LAST_JOURNAL_ENTRY_MADE:
           return anotherObservationItemProxy.item.createdOn -
             oneObservationItemProxy.item.createdOn;
-        case Ordering.OBSERVER:
+        case JournalOrdering.OBSERVER:
           return oneObservationItemProxy.item.observedBy.localeCompare(
             anotherObservationItemProxy.item.observedBy);
-        case Ordering.JOURNAL_ENTRY_MAKER:
+        case JournalOrdering.JOURNAL_ENTRY_MAKER:
           return oneObservationItemProxy.item.createdBy.localeCompare(
             anotherObservationItemProxy.item.createdBy);
-        case Ordering.ISSUES_FIRST:
+        case JournalOrdering.ISSUES_FIRST:
           return oneObservationItemProxy.kind.localeCompare(
             anotherObservationItemProxy.kind);
-        case Ordering.ISSUES_LAST:
+        case JournalOrdering.ISSUES_LAST:
           return anotherObservationItemProxy.kind.localeCompare(
             oneObservationItemProxy.kind);
       }
     });
   }
-  
+
   public getViewModel(typeName: string): any {
     return TreeConfiguration.getWorkingTree().getProxyFor('view-' + typeName.
       toLowerCase()).item;
   }
-  
+
   public save(itemProxy: ItemProxy): void {
     this._itemRepository.upsertItem(itemProxy.kind, itemProxy.item).then(
       (returnedItemProxy: ItemProxy) => {
@@ -200,14 +202,14 @@ export class JournalComponent {
     });
     this._editableSet.splice(this._editableSet.indexOf(itemProxy.item.id), 1);
   }
-  
+
   public discardChanges(itemProxy: ItemProxy): void {
     this._itemRepository.fetchItem(TreeConfiguration.getWorkingTree().
       getProxyFor(itemProxy.item.id));
     this._editableSet.splice(this._editableSet.indexOf(itemProxy.item.id), 1);
     this._changeDetectorRef.markForCheck();
   }
-  
+
   public navigate(itemProxy: ItemProxy): void {
     this._navigationService.addTab('Explore', { id: itemProxy.item.id });
   }
