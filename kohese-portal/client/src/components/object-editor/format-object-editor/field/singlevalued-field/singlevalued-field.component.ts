c2160cd3ac1969ab4290997121a9e9fbadfd8a80
dstockman <dstockman@cohesionforce.com>
2020-10-01 15:13:22 -0500

    Added capability to unset username and date fields

--- kohese-portal/client/src/components/object-editor/format-object-editor/field/singlevalued-field/singlevalued-field.component.ts
+++ kohese-portal/client/src/components/object-editor/format-object-editor/field/singlevalued-field/singlevalued-field.component.ts
@@ -21,4 +21,18 @@ export class SinglevaluedFieldComponent extends Field {
     SessionService) {
     super(changeDetectorRef, itemRepository, dialogService, sessionService);
   }
+
+  /**
+   * Returns whether the given username values are considered equal
+   * 
+   * @param option A value option
+   * @param selection The current value
+   */
+  public areUsernamesEqual(option: string, selection: string): boolean {
+    if (!option || !selection) {
+      return (!option && !selection);
+    } else {
+      return (option === selection);
+    }
+  }
 }
