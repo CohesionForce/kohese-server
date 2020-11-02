d27c7b610408fca9f416b5618b0a75a8bbc6412a
Thomas Pitts <tpitts@cohesionforce.com>
2020-11-02 17:08:54 -0600

    Resolve Version Tree Issues Task: Resolve Version Tree Issues (0fca5fa0-14b1-11eb-aec0-2ba7238903b2) * Horizontal split * Show only differences toggle checkbox * Show comparison information while viewing children

--- kohese-portal/client/src/components/versions/versions.component.ts
+++ kohese-portal/client/src/components/versions/versions.component.ts
@@ -1,5 +1,4 @@
-import { Component, ChangeDetectionStrategy,
-  ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 import { Commit } from '../tree/commit-tree/commit-tree.component';
@@ -16,24 +15,24 @@ export class VersionsComponent {
   get selectedVersionObject() {
     return this._selectedVersionObject;
   }
-  
-  private _comparisonsSubject: BehaviorSubject<Array<Comparison>> =
-    new BehaviorSubject<Array<Comparison>>([]);
+
+  private _comparisonsSubject: BehaviorSubject<Array<Comparison>> = new BehaviorSubject<Array<Comparison>>([]);
   get comparisonsSubject() {
     return this._comparisonsSubject;
   }
-  
-  get showDifferencesOnly() {
-    return true;
+
+  private _showDifferencesOnlySubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  get showDifferencesOnlySubject() {
+    return this._showDifferencesOnlySubject;
   }
-  
+
   public constructor(private _changeDetectorRef: ChangeDetectorRef) {
   }
-  
+
   public commitTreeRowSelected(object: any): void {
     this._selectedVersionObject = object;
   }
-  
+
   public getSelectionType(): string {
     let type: string = '';
     if (this._selectedVersionObject instanceof Commit) {
@@ -42,7 +41,12 @@ export class VersionsComponent {
     } else if (this._selectedVersionObject instanceof Comparison) {
       type = 'Comparison';
     }
-    
+
     return type;
+  }
+
+  public toggleShowingDifferencesOnly(): void {
+    this._showDifferencesOnlySubject.next(!this._showDifferencesOnlySubject.getValue());
+    this._changeDetectorRef.markForCheck();
   }
 }
