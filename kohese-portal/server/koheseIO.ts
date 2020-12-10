3a43884ce250b5b589410e78306b59e51b51076b
Thomas Pitts <tpitts@cohesionforce.com>
2020-12-10 15:16:17 -0600

    edited console log for newSession and connectionRemoved

--- kohese-portal/server/koheseIO.ts
+++ kohese-portal/server/koheseIO.ts
@@ -57,7 +57,7 @@ function Server(httpsServer, options){
               kio.sessions[data.id].numberOfConnections++;
               console.log('::: session %s for user %s added tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
             } else {
-              console.log('*** session %s for user %s attempted to increment connection count before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
+              console.log('*** session %s for user %s attempted to increment connection count for tab %s before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
             }
           });
           socket.on('connectionRemoved', (data: any, sendResponse: () => void) => {
@@ -65,7 +65,7 @@ function Server(httpsServer, options){
               kio.sessions[data.id].numberOfConnections--;
               console.log('::: session %s for user %s removed tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
             } else {
-              console.log('*** session %s for user %s attempted to decrement connection count before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
+              console.log('*** session %s for user %s attempted to decrement connection count for tab %s before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
             }
           });
           socket.on('getSessionMap', (data: any, sendResponse: (data:
