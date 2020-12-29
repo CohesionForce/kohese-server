c524987f314b7a431f7812cb8fc36b8546edc54f
Thomas Pitts <tpitts@cohesionforce.com>
2020-12-29 16:32:33 -0600

    Navigation from References in all views within Format Object Editor Task: Navigation from References in all views within Format Object Editor (e05a7750-08da-11eb-9bfe-cbac2c5aa40a)

--- kohese-portal/client/src/components/object-editor/format-object-editor/field/singlevalued-field/singlevalued-field.component.ts
+++ kohese-portal/client/src/components/object-editor/format-object-editor/field/singlevalued-field/singlevalued-field.component.ts
@@ -4,6 +4,8 @@ import { ChangeDetectionStrategy, ChangeDetectorRef,
 import { DialogService } from '../../../../../services/dialog/dialog.service';
 import { ItemRepository } from '../../../../../services/item-repository/item-repository.service';
 import { SessionService } from '../../../../../services/user/session.service';
+import { NavigationService } from '../../../../../services/navigation/navigation.service';
+import { DetailsComponent } from '../../../../details/details.component';
 import { Field } from '../field.class';
 
 /**
@@ -18,13 +20,13 @@ import { Field } from '../field.class';
 export class SinglevaluedFieldComponent extends Field {
   public constructor(changeDetectorRef: ChangeDetectorRef, itemRepository:
     ItemRepository, dialogService: DialogService, sessionService:
-    SessionService) {
+    SessionService, private navigationService: NavigationService) {
     super(changeDetectorRef, itemRepository, dialogService, sessionService);
   }
 
   /**
    * Returns whether the given username values are considered equal
-   * 
+   *
    * @param option A value option
    * @param selection The current value
    */
@@ -35,4 +37,35 @@ export class SinglevaluedFieldComponent extends Field {
       return (option === selection);
     }
   }
+
+  public getReferenceId(): string {
+    let referenceId = undefined;
+    if (this.propertyDefinition.kind === 'proxy-selector') {
+      let reference = this.koheseObject[this.propertyDefinition.propertyName];
+      if (reference.id) {
+        referenceId = reference.id;
+      } else {
+        referenceId = reference;
+      }
+    }
+
+    return referenceId;
+  }
+
+  public displayInformation(): void {
+    let itemProxy = this.itemRepository.getTreeConfig().getValue().config.getProxyFor(this.getReferenceId());
+    if (itemProxy) {
+      this._dialogService.openComponentDialog(DetailsComponent, { data: { itemProxy: itemProxy } }).updateSize('90%', '90%');
+    } else {
+      this._dialogService.openInformationDialog(
+        'Item not Found',
+        'Item not found for: ' +
+          this.itemRepository.getStringRepresentation(this.koheseObject,
+            this.propertyDefinition.propertyName, undefined, (this.enclosingDataModel ? this.enclosingDataModel : this.dataModel),
+            this.dataModel, this.viewModel,this.formatDefinitionType) + '.'
+      );
+    }
+
+  }
+
 }
