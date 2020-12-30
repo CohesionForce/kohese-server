28c91e08314a8a6c50d012931e9190aaaa88a1c6
Thomas Pitts <tpitts@cohesionforce.com>
2020-12-30 15:22:23 -0600

    Fix issue where Description field re-positions to top of field when user is typing. Task: Fix issue where Description field re-positions to top of field when user is typing. (16a08a10-a027-11ea-a274-37046e4e2702) * Made additions to markdown module for *ngIf capabilities

--- kohese-portal/client/src/components/markdown-editor/markdown-editor.module.ts
+++ kohese-portal/client/src/components/markdown-editor/markdown-editor.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { MaterialModule } from '../../material.module';
@@ -10,6 +12,8 @@ import { MarkdownEditorComponent } from './markdown-editor.component';
   exports: [MarkdownEditorComponent],
   imports: [
     FormsModule,
+    CommonModule,
+    BrowserModule,
     MarkdownModule,
     MaterialModule
   ]
