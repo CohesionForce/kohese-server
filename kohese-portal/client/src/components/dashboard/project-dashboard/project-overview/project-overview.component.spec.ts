d9db4c4f29460af3d44eb1c1fa6f65883b814697
Thomas Pitts <tpitts@cohesionforce.com>
2021-05-17 07:55:56 -0500

    fixed ProjectOverView spec

--- kohese-portal/client/src/components/dashboard/project-dashboard/project-overview/project-overview.component.spec.ts
+++ kohese-portal/client/src/components/dashboard/project-dashboard/project-overview/project-overview.component.spec.ts
@@ -1,30 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { of as ObservableOf } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-import { DashboardModule } from '../../dashboard.module';
+import { MaterialModule } from '../../../../material.module';
+import { PipesModule } from '../../../../pipes/pipes.module';
+import { ItemRepository } from '../../../../services/item-repository/item-repository.service';
+import { MockItemRepository } from '../../../../../mocks/services/MockItemRepository';
+import { DynamicTypesService } from '../../../../services/dynamic-types/dynamic-types.service';
 import { DialogService } from '../../../../services/dialog/dialog.service';
 import { MockDialogService } from '../../../../../mocks/services/MockDialogService';
-import { TreeConfiguration } from '../../../../../../common/src/tree-configuration';
+import { StateFilterService } from '../../state-filter.service';
+import { MockStateFilterService } from '../../../../../mocks/services/MockStateFilterService';
+import { ProjectInfo } from '../../../../services/project-service/project.service';
+import { MockProjectService } from '../../../../../mocks/services/MockProjectService';
 import { ProjectOverviewComponent } from './project-overview.component';
 
-describe('ProjectOverviewComponent', () => {
+describe('Component: StatusDashboardComponent', () => {
   let component: ProjectOverviewComponent;
-  let fixture: ComponentFixture<ProjectOverviewComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ DashboardModule ],
-      providers: [ { provide: DialogService, useClass: MockDialogService } ]
+      declarations: [ ProjectOverviewComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        MaterialModule,
+        PipesModule
+        ],
+      providers: [
+        { provide: ItemRepository, useClass: MockItemRepository },
+          DynamicTypesService,
+        { provide: DialogService, useClass: MockDialogService },
+        { provide: StateFilterService, useClass: MockStateFilterService }
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     }).compileComponents();
-    
-    fixture = TestBed.createComponent(ProjectOverviewComponent);
+
+    let fixture: ComponentFixture<ProjectOverviewComponent> = TestBed.createComponent(ProjectOverviewComponent);
     component = fixture.componentInstance;
-    component.projectStream = ObservableOf({
-      proxy: TreeConfiguration.getWorkingTree().getProxyFor('test-uuid1'),
-      users: [],
-      projectItems: []
-    });
-    
+    component.projectStream = new BehaviorSubject<ProjectInfo>(new MockProjectService().getProjects()[0]);
     fixture.detectChanges();
   });
 
