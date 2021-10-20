9b5a5999a79bebad7c839290f8d675c56700ebd2
Thomas Pitts <tpitts@cohesionforce.com>
2021-10-20 14:13:27 -0500

    consolidated drop() functionality

--- kohese-portal/client/src/components/tree/tree.component.ts
+++ kohese-portal/client/src/components/tree/tree.component.ts
@@ -704,23 +704,9 @@ export class TreeComponent implements OnInit, AfterViewInit, Dialog {
   /**
    * @event Drag&Drop connected sorting group
    */
-   dropFavorites(event: CdkDragDrop<any>) {
+   drop(array: Array<any>, event: CdkDragDrop<any>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(this.favorites, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
-    }
-  }
-
-  /**
-   * @event Drag&Drop connected sorting group
-   */
-   dropSelected(event: CdkDragDrop<any>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(this.selection, event.previousIndex, event.currentIndex);
+      moveItemInArray(array, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
                         event.container.data,
