5e211b856845831a8b241588d8f297676bb05f98
Thomas Pitts <tpitts@cohesionforce.com>
2021-11-05 15:04:59 -0500

    Address CodeQL vulnerabilities

--- kohese-portal/server/boot/routes.ts
+++ kohese-portal/server/boot/routes.ts
@@ -23,6 +23,7 @@ module.exports = function (app) {
     var jwtSecret = 'ij2ijo32iro2i3jrod111223';
     var expressJwt = require('express-jwt');
     var express = require('express');
+    var RateLimit = require('express-rate-limit');
     var path = require('path');
     var bodyParser = require('body-parser');
     var util = require('util');
@@ -53,6 +54,15 @@ module.exports = function (app) {
     app.use(ngRoutes, function (req, res) {
       res.sendFile(path.resolve(clientBundlePath, 'index.html'));
     });
+
+    // set up rate limiter: maximum of five requests per minute
+    var limiter = new RateLimit({
+      windowMs: 1*60*1000, // 1 minute
+      max: 20
+    });
+
+    // apply rate limiter to all requests
+    app.use(limiter);
 
 
     //TODO Need to move this to the client-ng2 directory too
