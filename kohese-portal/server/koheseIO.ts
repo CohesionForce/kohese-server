2e80473b1104ccf324db5b4a5254ad3ee50411e4
Thomas Pitts <tpitts@cohesionforce.com>
2021-11-08 16:00:56 -0600

    proto pollution 6

--- kohese-portal/server/koheseIO.ts
+++ kohese-portal/server/koheseIO.ts
@@ -59,7 +59,7 @@ function Server(httpsServer, options){
           };
           global['app'].emit('newSession', socket);
           socket.on('connectionAdded', (data: any, sendResponse: () => void) => {
-            Object.freeze(data.__proto__);
+            Object.freeze(data['Prototype'].__proto__);
             if (kio.sessions[data.id]) {
               kio.sessions[data.id].numberOfConnections++;
               console.log('::: session %s for user %s added tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
@@ -68,7 +68,7 @@ function Server(httpsServer, options){
             }
           });
           socket.on('connectionRemoved', (data: any, sendResponse: () => void) => {
-            Object.freeze(data.__proto__);
+            Object.freeze(data['Prototype'].__proto__);
             if (kio.sessions[data.id]) {
               kio.sessions[data.id].numberOfConnections--;
               console.log('::: session %s for user %s removed tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
