c2a5ca29cc4a75c349deb3aa1b98fe84f1695cbe
Thomas Pitts <tpitts@cohesionforce.com>
2021-11-09 10:39:01 -0600

    proto pollution 11

--- kohese-portal/server/koheseIO.ts
+++ kohese-portal/server/koheseIO.ts
@@ -59,25 +59,17 @@ function Server(httpsServer, options){
           };
           global['app'].emit('newSession', socket);
           socket.on('connectionAdded', (data: any, sendResponse: () => void) => {
-            if (kio.sessions[data.id]) {
-              if(kio.sessions[data.id] !== '__proto__') {
-                kio.sessions[data.id].numberOfConnections++;
-                console.log('::: session %s for user %s added tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
-              } else {
-                return;
-              }
+            if (kio.sessions[data.id] && kio.sessions[data.id] !== '__proto__') {
+              kio.sessions[data.id].numberOfConnections++;
+              console.log('::: session %s for user %s added tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
             } else {
               console.log('*** session %s for user %s attempted to increment connection count for tab %s before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
             }
           });
           socket.on('connectionRemoved', (data: any, sendResponse: () => void) => {
-            if (kio.sessions[data.id]) {
-              if(kio.sessions[data.id] !== '__proto__') {
-                kio.sessions[data.id].numberOfConnections--;
-                console.log('::: session %s for user %s removed tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
-              } else {
-                return;
-              }
+            if (kio.sessions[data.id] && kio.sessions[data.id] !== '__proto__') {
+              kio.sessions[data.id].numberOfConnections--;
+              console.log('::: session %s for user %s removed tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
             } else {
               console.log('*** session %s for user %s attempted to decrement connection count for tab %s before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
             }
