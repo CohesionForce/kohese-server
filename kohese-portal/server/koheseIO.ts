69a5ff5d9fd5c6f55851e873be92eeca0329d68b
Thomas Pitts <tpitts@cohesionforce.com>
2021-11-09 10:44:51 -0600

    proto poll 12

--- kohese-portal/server/koheseIO.ts
+++ kohese-portal/server/koheseIO.ts
@@ -59,7 +59,7 @@ function Server(httpsServer, options){
           };
           global['app'].emit('newSession', socket);
           socket.on('connectionAdded', (data: any, sendResponse: () => void) => {
-            if (kio.sessions[data.id] && kio.sessions[data.id] !== '__proto__') {
+            if (kio.sessions[data.id] && kio.sessions[data.id] !== '__proto__' && kio.sessions[data] !== '__proto__') {
               kio.sessions[data.id].numberOfConnections++;
               console.log('::: session %s for user %s added tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
             } else {
@@ -67,7 +67,7 @@ function Server(httpsServer, options){
             }
           });
           socket.on('connectionRemoved', (data: any, sendResponse: () => void) => {
-            if (kio.sessions[data.id] && kio.sessions[data.id] !== '__proto__') {
+            if (kio.sessions[data.id] && kio.sessions[data.id] !== '__proto__' && kio.sessions[data] !== '__proto__') {
               kio.sessions[data.id].numberOfConnections--;
               console.log('::: session %s for user %s removed tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
             } else {
