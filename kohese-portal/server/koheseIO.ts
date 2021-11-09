8cca3f584a03992ce8764d8b580900dedb32e907
Thomas Pitts <tpitts@cohesionforce.com>
2021-11-09 11:39:52 -0600

    proto 13

--- kohese-portal/server/koheseIO.ts
+++ kohese-portal/server/koheseIO.ts
@@ -60,7 +60,7 @@ function Server(httpsServer, options){
           global['app'].emit('newSession', socket);
           socket.on('connectionAdded', (data: any, sendResponse: () => void) => {
 
-            if (kio.sessions[data.id] !== '__proto__') {
+            if (kio.sessions[data.id]) {
               kio.sessions[data.id].numberOfConnections++;
               console.log('::: session %s for user %s added tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
             } else {
@@ -70,12 +70,18 @@ function Server(httpsServer, options){
 
           socket.on('connectionRemoved', (data: any, sendResponse: () => void) => {
 
-            if (kio.sessions[data.id] !== '__proto__') {
-              kio.sessions[data.id].numberOfConnections--;
-              console.log('::: session %s for user %s removed tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
-            } else {
-              console.log('*** session %s for user %s attempted to decrement connection count for tab %s before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
+            for(let attr in data) {
+              if(attr === '__proto__') {
+                continue;
+              } else if (attr[data.id] === kio.sessions[data.id]) {
+                kio.sessions[data.id].numberOfConnections--;
+                console.log('::: session %s for user %s removed tab %s for a total of %s', socket.id, socket.koheseUser.username, data.clientTabId, kio.sessions[data.id].numberOfConnections);
+              } else {
+                console.log('*** session %s for user %s attempted to decrement connection count for tab %s before establishing session.', socket.id, socket.koheseUser.username, data.clientTabId);
+              }
             }
+
+
           });
           socket.on('getSessionMap', (data: any, sendResponse: (data:
             any) => void) => {
