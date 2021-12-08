43b17ffffcbf0854448608c8df8690fa9264b5f8
Thomas Pitts <tpitts@cohesionforce.com>
2021-12-08 11:49:56 -0600

    removed test description focus flag

--- kohese-portal/client/src/components/tree/tree.component.spec.ts
+++ kohese-portal/client/src/components/tree/tree.component.spec.ts
@@ -35,7 +35,7 @@ import { TreeComponent } from './tree.component';
 import { MockItemRepository } from '../../../mocks/services/MockItemRepository';
 import { MockItem, MockAction } from '../../../mocks/data/MockItem';
 
-fdescribe('TreeComponent', () => {
+describe('TreeComponent', () => {
   let component: TreeComponent;
 
   beforeEach(() => {
