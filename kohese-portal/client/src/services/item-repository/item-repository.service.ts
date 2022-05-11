1af2876a9857b7136363ff962b4f9b6aeb5168cf
Thomas Pitts <tpitts@cohesionforce.com>
2022-05-11 15:15:20 -0500

    Remove Non-Document Name Format from Cards

--- kohese-portal/client/src/services/item-repository/item-repository.service.ts
+++ kohese-portal/client/src/services/item-repository/item-repository.service.ts
@@ -757,8 +757,9 @@ export class ItemRepository {
 	        0].propertyName] + '](' + window.location.origin + LocationMap[
 	        'Explore'].route + ';id=' + koheseObject.id + ')\n\n');
   	  } else {
-	      representation += (koheseObject[formatDefinition.header.contents[0].
-	        propertyName] + '\n\n');
+        if(formatDefinitionType === FormatDefinitionType.DOCUMENT) {
+          representation += (koheseObject[formatDefinition.header.contents[0].propertyName] + '\n\n');
+        }
 	    }
 	  }
 
